2a99c4a5ad5f2ba5a2598c601e272bd98a1bc63a
Paul Bouquet <paul.bouquet@gmail.com>
2018-06-20 22:51:51 +0200

    Fix install of hybrid trading mode

--- Trading/Mode/hybrid_trading_mode.py
+++ Trading/Mode/hybrid_trading_mode.py
@@ -2,7 +2,7 @@
 OctoBot Tentacle
 
 $tentacle_description: {
-    "name": "hybrid_mode",
+    "name": "hybrid_trading_mode",
     "type": "Trading",
     "subtype": "Mode",
     "version": "1.0.0",
@@ -14,18 +14,18 @@ from trading.trader.modes.abstract_mode_decider import AbstractTradingModeDecide
 from trading.trader.modes.abstract_trading_mode import AbstractTradingMode
 
 
-class HybridMode(AbstractTradingMode):
+class HybridTradingMode(AbstractTradingMode):
     def __init__(self, config, symbol_evaluator, exchange):
         super().__init__(config, symbol_evaluator, exchange)
 
-        self.set_decider(HybridModeDecider(self, symbol_evaluator, exchange))
+        self.set_decider(HybridTradingModeDecider(self, symbol_evaluator, exchange))
 
     @staticmethod
     def get_required_strategies():
         return [MarketStabilityStrategiesEvaluator]
 
 
-class HybridModeDecider(AbstractTradingModeDecider):
+class HybridTradingModeDecider(AbstractTradingModeDecider):
     def __init__(self, trading_mode, symbol_evaluator, exchange):
         super().__init__(trading_mode, symbol_evaluator, exchange)
 
