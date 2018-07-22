37cb656de4cdf316dca365f70c5e079183768134
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2018-07-22 14:25:37 +0200

    added mul mixed strategy default run test

--- tests/Evaluator/Strategies/test_full_mixed_strategies_evaluator.py
+++ tests/Evaluator/Strategies/test_full_mixed_strategies_evaluator.py
@@ -1,1 +1,18 @@
+import pytest
 
+from tests.functional_tests.strategy_evaluators_tests.abstract_strategy_test import AbstractStrategyTest
+from evaluator.Strategies import FullMixedStrategiesEvaluator
+
+
+@pytest.fixture()
+def strategy_tester():
+    strategy_tester_instance = TestFullMixedStrategiesEvaluator()
+    strategy_tester_instance.init(FullMixedStrategiesEvaluator)
+    return strategy_tester_instance
+
+
+class TestFullMixedStrategiesEvaluator(AbstractStrategyTest):
+
+    @staticmethod
+    def test_default_run(strategy_tester):
+        strategy_tester.run_test_default_run(-12)
