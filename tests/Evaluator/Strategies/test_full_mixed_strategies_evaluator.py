a302478438ccd269ab196960c8a816a2c7587d2e
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2018-08-13 01:25:37 +0200

    fixed strategy test to match debugged backtesting timeframes update

--- tests/Evaluator/Strategies/test_full_mixed_strategies_evaluator.py
+++ tests/Evaluator/Strategies/test_full_mixed_strategies_evaluator.py
@@ -19,24 +19,24 @@ class TestFullMixedStrategiesEvaluator(AbstractStrategyTest):
 
     @staticmethod
     def test_slow_downtrend(strategy_tester):
-        strategy_tester.run_test_slow_downtrend(-12, -7, -21, 0)
+        strategy_tester.run_test_slow_downtrend(-12, -7.8, -21, 0)
 
     @staticmethod
     def test_sharp_downtrend(strategy_tester):
-        strategy_tester.run_test_sharp_downtrend(-15, -21)
+        strategy_tester.run_test_sharp_downtrend(-14, -26)
 
     @staticmethod
     def test_flat_markets(strategy_tester):
-        strategy_tester.run_test_flat_markets(-5, -2, 7, 6)
+        strategy_tester.run_test_flat_markets(-2, -0.5, 8.5, 9)
 
     @staticmethod
     def test_slow_uptrend(strategy_tester):
-        strategy_tester.run_test_slow_uptrend(-5, -4)
+        strategy_tester.run_test_slow_uptrend(-3, -4)
 
     @staticmethod
     def test_sharp_uptrend(strategy_tester):
-        strategy_tester.run_test_sharp_uptrend(6.5, 2)
+        strategy_tester.run_test_sharp_uptrend(8.5, 11.5)
 
     @staticmethod
     def test_up_then_down(strategy_tester):
-        strategy_tester.run_test_up_then_down(-11.5)
+        strategy_tester.run_test_up_then_down(-13)
