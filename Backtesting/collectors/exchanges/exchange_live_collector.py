ff76a15d0cd6f6188f0c9a5cb2a2cd9247047d1d
Herklos <herklos.dev@protonmail.com>
2020-01-05 18:02:04 +0100

    [ExchangeId] Add exchange id support

--- Backtesting/collectors/exchanges/exchange_live_collector.py
+++ Backtesting/collectors/exchanges/exchange_live_collector.py
@@ -49,22 +49,22 @@ class ExchangeLiveDataCollector(ExchangeDataCollector):
 
         await asyncio.gather(*asyncio.all_tasks(asyncio.get_event_loop()))
 
-    async def ticker_callback(self, exchange, symbol, ticker):
+    async def ticker_callback(self, exchange: str, exchange_id: str, symbol: str, ticker):
         self.logger.info(f"TICKER : SYMBOL = {symbol} || TICKER = {ticker}")
         await self.save_ticker(timestamp=time.time(), exchange=exchange, symbol=symbol, ticker=ticker)
 
-    async def order_book_callback(self, exchange, symbol, asks, bids):
+    async def order_book_callback(self, exchange: str, exchange_id: str, symbol: str, asks, bids):
         self.logger.info(f"ORDERBOOK : SYMBOL = {symbol} || ASKS = {asks} || BIDS = {bids}")
         await self.save_order_book(timestamp=time.time(), exchange=exchange, symbol=symbol, asks=asks, bids=bids)
 
-    async def recent_trades_callback(self, exchange, symbol, recent_trades):
+    async def recent_trades_callback(self, exchange: str, exchange_id: str, symbol: str, recent_trades):
         self.logger.info(f"RECENT TRADE : SYMBOL = {symbol} || RECENT TRADE = {recent_trades}")
         await self.save_recent_trades(timestamp=time.time(), exchange=exchange, symbol=symbol, recent_trades=recent_trades)
 
-    async def ohlcv_callback(self, exchange, symbol, time_frame, candle):
+    async def ohlcv_callback(self, exchange: str, exchange_id: str, symbol: str, time_frame, candle):
         self.logger.info(f"OHLCV : SYMBOL = {symbol} || TIME FRAME = {time_frame} || CANDLE = {candle}")
         await self.save_ohlcv(timestamp=time.time(), exchange=exchange, symbol=symbol, time_frame=time_frame, candle=candle)
 
-    async def kline_callback(self, exchange, symbol, time_frame, kline):
+    async def kline_callback(self, exchange: str, exchange_id: str, symbol: str, time_frame, kline):
         self.logger.info(f"KLINE : SYMBOL = {symbol} || TIME FRAME = {time_frame} || KLINE = {kline}")
         await self.save_kline(timestamp=time.time(), exchange=exchange, symbol=symbol, time_frame=time_frame, kline=kline)
