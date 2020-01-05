913fa1be1932d93870b69d221883cc60c748631d
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-01-05 22:41:29 +0100

    migrate web interface

---
+++ Interfaces/web/constants.py
@@ -0,0 +1,40 @@
+#  Drakkar-Software OctoBot-Interfaces
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+
+
+# utility URLs
+COIN_MARKET_CAP_CURRENCIES_LIST_URL = "https://api.coinmarketcap.com/v2/listings/"
+
+# config keys
+CONFIG_WATCHED_SYMBOLS = "watched_symbols"
+
+# web interface keys
+GLOBAL_CONFIG_KEY = "global_config"
+EVALUATOR_CONFIG_KEY = "evaluator_config"
+DEACTIVATE_OTHERS = "deactivate_others"
+TRADING_CONFIG_KEY = "trading_config"
+UPDATED_CONFIG_SEPARATOR = "_"
+EVALUATOR_ACTIVATION = "activation"
+
+# web display
+DEFAULT_TIMEFRAME = "1h"
+
+# backtesting
+BOT_TOOLS_BACKTESTING = "backtesting"
+BOT_TOOLS_BACKTESTING_SOURCE = "backtesting_source"
+
+# strategy optimizer
+BOT_TOOLS_STRATEGY_OPTIMIZER = "strategy_optimizer"
---
+++ Interfaces/web/web_interface.py
@@ -0,0 +1,82 @@
+#  Drakkar-Software OctoBot-Interfaces
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+from werkzeug.serving import make_server
+
+import threading
+from time import sleep
+
+from octobot_services.constants import CONFIG_WEB, CONFIG_CATEGORY_SERVICES, CONFIG_WEB_IP, CONFIG_WEB_PORT
+from tentacles.Interfaces.web import server_instance
+from tentacles.Interfaces.web.constants import BOT_TOOLS_BACKTESTING, BOT_TOOLS_BACKTESTING_SOURCE, \
+    BOT_TOOLS_STRATEGY_OPTIMIZER
+from tentacles.Interfaces.web.controllers import load_routes
+from tentacles.Services import WebService
+from octobot_interfaces.web.abstract_web_interface import AbstractWebInterface
+
+
+class WebInterface(AbstractWebInterface, threading.Thread):
+    REQUIRED_SERVICE = WebService
+
+    tools = {
+        BOT_TOOLS_BACKTESTING: None,
+        BOT_TOOLS_BACKTESTING_SOURCE: None,
+        BOT_TOOLS_STRATEGY_OPTIMIZER: None
+    }
+
+    def __init__(self, config):
+        AbstractWebInterface.__init__(self, config)
+        threading.Thread.__init__(self)
+        self.app = None
+        self.srv = None
+        self.ctx = None
+
+    def _prepare_server(self):
+        try:
+            self.srv = make_server(host=self.config[CONFIG_CATEGORY_SERVICES][CONFIG_WEB][CONFIG_WEB_IP],
+                                   port=self.config[CONFIG_CATEGORY_SERVICES][CONFIG_WEB][CONFIG_WEB_PORT],
+                                   threaded=True,
+                                   app=server_instance)
+            self.ctx = server_instance.app_context()
+            self.ctx.push()
+        except OSError as e:
+            self.srv = None
+            self.get_logger().exception(f"Fail to start web interface : {e}")
+
+    async def _async_run(self) -> bool:
+        # wait bot is ready
+        while not self.is_bot_ready():
+            sleep(0.1)
+
+        # Define the WSGI server object
+        self._prepare_server()
+
+        load_routes()
+
+        if self.srv:
+            self.srv.serve_forever()
+            return True
+        return False
+
+    async def _inner_start(self) -> bool:
+        threading.Thread.start(self)
+        return True
+
+    def _prepare_stop(self):
+        self.srv.server_close()
+
+    def stop(self):
+        self._prepare_stop()
+        self.srv.shutdown()
