b93d10c33db6bb7e9832ae5787e46765b307e969
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-02-11 21:48:58 +0100

    handle v3 data file conversion

--- Backtesting/converters/exchanges/__init__.py
+++ Backtesting/converters/exchanges/__init__.py
@@ -1,4 +1,4 @@
-#  Drakkar-Software OctoBot-Backtesting
+#  Drakkar-Software OctoBot-Tentacles
 #  Copyright (c) Drakkar-Software, All rights reserved.
 #
 #  This library is free software; you can redistribute it and/or
