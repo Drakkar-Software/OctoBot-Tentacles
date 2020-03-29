a4214db8a4fa254c3d41d94e7d2b676e78992972
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-03-30 01:41:10 +0200

    cleanup

--- Websockets/feeds/__init__.py
+++
@@ -1,8 +0,0 @@
-from octobot_tentacles_manager.api.inspector import check_tentacle_version
-from octobot_commons.logging.logging_util import get_logger
-
-if check_tentacle_version('1.2.0', 'bitmex_feed', 'OctoBot-Default-Tentacles'):
-    try:
-        from .bitmex_feed import *
-    except Exception as e:
-        get_logger('TentacleLoader').exception(e, True, f'Error when loading bitmex_feed: {e}')
