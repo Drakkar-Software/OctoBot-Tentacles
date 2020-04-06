b6f2f1313b40bf837dc76868d88e443df05cf485
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-04-06 18:23:53 +0200

    migrate instant fluctuation evaluators

---
+++ Evaluator/RealTime/instant_fluctuations_evaluator/__init__.py
@@ -0,0 +1,1 @@
+from .instant_fluctuations_evaluator import InstantFluctuationsEvaluator, InstantMAEvaluator
---
+++ Evaluator/RealTime/price_refresher_evaluator/__init__.py
@@ -0,0 +1,1 @@
+# from .price_refresher_evaluator import StochasticRSIVolatilityEvaluator
