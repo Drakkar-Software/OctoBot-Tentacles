8fd3ffaca5b4c318d9f7dc1601406de8f23a93c4
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-04-24 00:29:23 +0200

    migrate mixed strategies evaluator

--- Evaluator/Strategies/mixed_strategies_evaluator/mixed_strategies_evaluator.py
+++ Evaluator/Strategies/mixed_strategies_evaluator/mixed_strategies_evaluator.py
@@ -14,20 +14,20 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 from octobot_commons.constants import START_PENDING_EVAL_NOTE, HOURS_TO_SECONDS
-from octobot_commons.enums import TimeFrames
 from octobot_commons.evaluators_util import check_valid_eval_note
+from octobot_commons.time_frame_manager import parse_time_frames
 from octobot_evaluators.api.matrix import get_value, get_type, get_time
 from octobot_evaluators.channels.evaluator_channel import trigger_technical_evaluators_re_evaluation_with_updated_data
 from octobot_evaluators.data_manager.matrix_manager import get_evaluations_by_evaluator
 from octobot_evaluators.enums import EvaluatorMatrixTypes
 from octobot_evaluators.errors import UnsetTentacleEvaluation
-from octobot_evaluators.evaluator import StrategyEvaluator, EVALUATOR_EVAL_DEFAULT_TYPE
+from octobot_evaluators.evaluator import StrategyEvaluator, EVALUATOR_EVAL_DEFAULT_TYPE, CONFIG_FORCED_TIME_FRAME
 from octobot_tentacles_manager.api.configurator import get_tentacle_config
 from octobot_trading.api.exchange import get_exchange_id_from_matrix_id, get_exchange_current_time, \
     get_exchange_manager_from_exchange_name_and_id
 
 
-class SimpleMixedStrategyEvaluator(StrategyEvaluator):
+class SimpleStrategyEvaluator(StrategyEvaluator):
 
     SOCIAL_EVALUATORS_NOTIFICATION_TIMEOUT_KEY = "social_evaluators_notification_timeout"
     RE_EVAL_TA_ON_RT_OR_SOCIAL = "re_evaluate_TA_when_social_or_realtime_notification"
@@ -35,16 +35,14 @@ class SimpleMixedStrategyEvaluator(StrategyEvaluator):
 
     def __init__(self):
         super().__init__()
-        self.counter = 0
-        self.evaluation = 0
         self.re_evaluation_triggering_eval_types = [EvaluatorMatrixTypes.SOCIAL.value,
                                                     EvaluatorMatrixTypes.REAL_TIME.value]
         config = get_tentacle_config(self.__class__)
         self.social_evaluators_default_timeout = config.get(
-            SimpleMixedStrategyEvaluator.SOCIAL_EVALUATORS_NOTIFICATION_TIMEOUT_KEY, 1 * HOURS_TO_SECONDS)
+            SimpleStrategyEvaluator.SOCIAL_EVALUATORS_NOTIFICATION_TIMEOUT_KEY, 1 * HOURS_TO_SECONDS)
         self.re_evaluate_TA_when_social_or_realtime_notification = config.get(
-            SimpleMixedStrategyEvaluator.RE_EVAL_TA_ON_RT_OR_SOCIAL, True)
-        self.background_social_evaluators = config.get(SimpleMixedStrategyEvaluator.BACKGROUND_SOCIAL_EVALUATORS, [])
+            SimpleStrategyEvaluator.RE_EVAL_TA_ON_RT_OR_SOCIAL, True)
+        self.background_social_evaluators = config.get(SimpleStrategyEvaluator.BACKGROUND_SOCIAL_EVALUATORS, [])
 
     async def matrix_callback(self,
                               matrix_id,
@@ -66,8 +64,7 @@ class SimpleMixedStrategyEvaluator(StrategyEvaluator):
                                                eval_note_type,
                                                exchange_name,
                                                cryptocurrency,
-                                               available_symbol,
-                                               time_frame)
+                                               available_symbol)
             return
         else:
             await self._trigger_evaluation(matrix_id,
@@ -77,8 +74,7 @@ class SimpleMixedStrategyEvaluator(StrategyEvaluator):
                                            eval_note_type,
                                            exchange_name,
                                            cryptocurrency,
-                                           symbol,
-                                           time_frame)
+                                           symbol)
 
     async def _trigger_evaluation(self,
                                   matrix_id,
@@ -88,8 +84,7 @@ class SimpleMixedStrategyEvaluator(StrategyEvaluator):
                                   eval_note_type,
                                   exchange_name,
                                   cryptocurrency,
-                                  symbol,
-                                  time_frame):
+                                  symbol):
         # ensure only start evaluations when technical evaluators have been initialized
         try:
             TA_by_timeframe = {
@@ -148,24 +143,24 @@ class SimpleMixedStrategyEvaluator(StrategyEvaluator):
                                                                                        self.strategy_time_frames)
                     # do not continue this evaluation
                     return
-            self.counter = 0
-            self.evaluation = 0
+            counter = 0
+            total_evaluation = 0
 
             for eval_by_rt in RT_evaluations_by_time_frame.values():
                 for evaluation in eval_by_rt.values():
                     eval_value = get_value(evaluation)
                     if check_valid_eval_note(eval_value, eval_type=get_type(evaluation),
                                              expected_eval_type=EVALUATOR_EVAL_DEFAULT_TYPE):
-                        self.evaluation += eval_value
-                        self.counter += 1
+                        total_evaluation += eval_value
+                        counter += 1
 
             for eval_by_ta in TA_by_timeframe.values():
                 for evaluation in eval_by_ta.values():
                     eval_value = get_value(evaluation)
                     if check_valid_eval_note(eval_value, eval_type=get_type(evaluation),
                                              expected_eval_type=EVALUATOR_EVAL_DEFAULT_TYPE):
-                        self.evaluation += eval_value
-                        self.counter += 1
+                        total_evaluation += eval_value
+                        counter += 1
 
             if social_evaluations_by_evaluator:
                 exchange_manager = get_exchange_manager_from_exchange_name_and_id(
@@ -180,11 +175,11 @@ class SimpleMixedStrategyEvaluator(StrategyEvaluator):
                                              eval_time=get_time(evaluation),
                                              expiry_delay=self.social_evaluators_default_timeout,
                                              current_time=current_time):
-                        self.evaluation += eval_value
-                        self.counter += 1
+                        total_evaluation += eval_value
+                        counter += 1
 
-            if self.counter > 0:
-                self.eval_note = self.evaluation / self.counter
+            if counter > 0:
+                self.eval_note = total_evaluation / counter
                 await self.evaluation_completed(cryptocurrency, symbol)
 
         except UnsetTentacleEvaluation as e:
@@ -195,18 +190,19 @@ class SimpleMixedStrategyEvaluator(StrategyEvaluator):
             self.logger.exception(e, True, f"Error when computing strategy evaluation: {e}")
 
 
-class FullMixedStrategiesEvaluator(StrategyEvaluator):
+class TechnicalAnalysisStrategyEvaluator(StrategyEvaluator):
+
+    TIME_FRAMES_TO_WEIGHT = "time_frames_to_weight"
+    TIME_FRAME = "time_frame"
+    WEIGHT = "weight"
+    DEFAULT_WEIGHT = 1
 
     def __init__(self):
         super().__init__()
-        self.social_counter = 0
-        self.ta_relevance_counter = 0
-        self.rt_counter = 0
-
-        self.ta_evaluation = 0
-        self.social_evaluation = 0
-        self.rt_evaluation = 0
-        self.divergence_evaluation = 0
+        self.allowed_evaluator_types = [EvaluatorMatrixTypes.TA.value, EvaluatorMatrixTypes.REAL_TIME.value]
+        config = get_tentacle_config(self.__class__)
+        self.weight_by_time_frames = TechnicalAnalysisStrategyEvaluator._get_weight_by_time_frames(
+            config[TechnicalAnalysisStrategyEvaluator.TIME_FRAMES_TO_WEIGHT])
 
     async def matrix_callback(self,
                               matrix_id,
@@ -214,94 +210,77 @@ class FullMixedStrategiesEvaluator(StrategyEvaluator):
                               evaluator_type,
                               eval_note,
                               eval_note_type,
-                              eval_time,
                               exchange_name,
                               cryptocurrency,
                               symbol,
                               time_frame):
-        # TODO: find better way than this if
-        if evaluator_name == self.get_name():
-            # avoid infinite auto callback
+        if evaluator_type not in self.allowed_evaluator_types:
+            # only wake up on relevant callbacks
             return
-        self.social_counter = 0
-        self.rt_counter = 0
 
-        # relevance counters
-        self.ta_relevance_counter = 0
+        try:
+            TA_by_timeframe = {
+                available_time_frame: get_evaluations_by_evaluator(
+                    matrix_id,
+                    exchange_name,
+                    EvaluatorMatrixTypes.TA.value,
+                    cryptocurrency,
+                    symbol,
+                    available_time_frame.value,
+                    allow_missing=False,
+                    allowed_values=[START_PENDING_EVAL_NOTE])
+                for available_time_frame in self.strategy_time_frames
+            }
 
-        # eval note total with relevance factor
-        self.ta_evaluation = 0
-        self.social_evaluation = 0
-        self.rt_evaluation = 0
+            if evaluator_type == EvaluatorMatrixTypes.REAL_TIME.value:
+                # trigger re-evaluation
+                exchange_id = get_exchange_id_from_matrix_id(exchange_name, matrix_id)
+                await trigger_technical_evaluators_re_evaluation_with_updated_data(matrix_id,
+                                                                                   evaluator_name,
+                                                                                   evaluator_type,
+                                                                                   exchange_name,
+                                                                                   cryptocurrency,
+                                                                                   symbol,
+                                                                                   exchange_id,
+                                                                                   self.strategy_time_frames)
+                # do not continue this evaluation
+                return
 
-        # example
-        # if RSIMomentumEvaluator.get_name() in self.matrix[EvaluatorMatrixTypes.TA]:
-        #     self.divergence_evaluation = self.divergence_evaluator_analyser.calc_evaluator_divergence(
-        #         EvaluatorMatrixTypes.TA,
-        #         RSIMomentumEvaluator.get_name())
+            total_evaluation = 0
+            total_weights = 0
 
-        for rt in self.matrix[EvaluatorMatrixTypes.REAL_TIME]:
-            if check_valid_eval_note(self.matrix[EvaluatorMatrixTypes.REAL_TIME][rt],
-                                     self.evaluator_types_matrix.get_evaluator_eval_type(rt),
-                                     EVALUATOR_EVAL_DEFAULT_TYPE):
-                self.rt_evaluation += self.matrix[EvaluatorMatrixTypes.REAL_TIME][rt]
-                self.rt_counter += 1
+            for time_frame, eval_by_ta in TA_by_timeframe.items():
+                for evaluation in eval_by_ta.values():
+                    eval_value = get_value(evaluation)
+                    if check_valid_eval_note(eval_value, eval_type=get_type(evaluation),
+                                             expected_eval_type=EVALUATOR_EVAL_DEFAULT_TYPE):
+                        weight = self.weight_by_time_frames.get(time_frame.value, self.DEFAULT_WEIGHT)
+                        total_evaluation += eval_value * weight
+                        total_weights += weight
 
-        for ta in self.matrix[EvaluatorMatrixTypes.TA]:
-            if self.matrix[EvaluatorMatrixTypes.TA][ta]:
-                for ta_time_frame in self.matrix[EvaluatorMatrixTypes.TA][ta]:
-                    if check_valid_eval_note(self.matrix[EvaluatorMatrixTypes.TA][ta][ta_time_frame],
-                                             self.evaluator_types_matrix.get_evaluator_eval_type(ta),
-                                             EVALUATOR_EVAL_DEFAULT_TYPE):
-                        time_frame_relevance = FullMixedStrategiesEvaluator.TimeFramesRelevance[ta_time_frame]
-                        self.ta_evaluation += self.matrix[EvaluatorMatrixTypes.TA][ta][ta_time_frame] \
-                            * time_frame_relevance
-                        self.ta_relevance_counter += time_frame_relevance
+            if total_weights > 0:
+                self.eval_note = total_evaluation / total_weights
+                await self.evaluation_completed(cryptocurrency, symbol)
 
-        for social in self.matrix[EvaluatorMatrixTypes.SOCIAL]:
-            eval_note = self.matrix[EvaluatorMatrixTypes.SOCIAL][social]
-            if check_valid_eval_note(eval_note,
-                                     self.evaluator_types_matrix.get_evaluator_eval_type(social),
-                                     EVALUATOR_EVAL_DEFAULT_TYPE):
-                self.social_evaluation += eval_note
-                self.social_counter += 1
+        except UnsetTentacleEvaluation as e:
+            self.logger.error(f"Missing technical evaluator data for ({e})")
 
-        await self.finalize(cryptocurrency, symbol)
+    @classmethod
+    def get_required_time_frames(cls, config: dict):
+        if CONFIG_FORCED_TIME_FRAME in config:
+            return parse_time_frames(config[CONFIG_FORCED_TIME_FRAME])
+        strategy_config = get_tentacle_config(cls)
+        if TechnicalAnalysisStrategyEvaluator.TIME_FRAMES_TO_WEIGHT in strategy_config:
+            tf_to_weight = strategy_config[TechnicalAnalysisStrategyEvaluator.TIME_FRAMES_TO_WEIGHT]
+            return parse_time_frames(TechnicalAnalysisStrategyEvaluator._get_weight_by_time_frames(tf_to_weight))
+        else:
+            raise Exception(f"'{TechnicalAnalysisStrategyEvaluator.TIME_FRAMES_TO_WEIGHT}' "
+                            f"is missing in configuration file")
 
-    async def finalize(self, cryptocurrency, symbol):
-        eval_temp = 0
-        category = 0
-
-        if self.ta_relevance_counter > 0:
-            eval_temp += self.ta_evaluation / self.ta_relevance_counter
-            category += 1
-
-        if self.social_counter > 0:
-            eval_temp += self.social_evaluation / self.social_counter
-            category += 1
-
-        if self.rt_counter > 0:
-            eval_temp += self.rt_evaluation / self.rt_counter
-            category += 1
-
-        if category > 0:
-            self.eval_note = eval_temp / category
-            self.eval_note = -1
-            await self.evaluation_completed(cryptocurrency, symbol)
-
-    TimeFramesRelevance = {
-        TimeFrames.ONE_MINUTE: 5,
-        TimeFrames.THREE_MINUTES: 5,
-        TimeFrames.FIVE_MINUTES: 5,
-        TimeFrames.FIFTEEN_MINUTES: 15,
-        TimeFrames.THIRTY_MINUTES: 30,
-        TimeFrames.ONE_HOUR: 50,
-        TimeFrames.TWO_HOURS: 50,
-        TimeFrames.FOUR_HOURS: 50,
-        TimeFrames.HEIGHT_HOURS: 30,
-        TimeFrames.TWELVE_HOURS: 30,
-        TimeFrames.ONE_DAY: 30,
-        TimeFrames.THREE_DAYS: 15,
-        TimeFrames.ONE_WEEK: 15,
-        TimeFrames.ONE_MONTH: 5,
-    }
+    @staticmethod
+    def _get_weight_by_time_frames(tf_to_weight):
+        return {
+            tf_and_weight[TechnicalAnalysisStrategyEvaluator.TIME_FRAME]:
+                tf_and_weight[TechnicalAnalysisStrategyEvaluator.WEIGHT]
+            for tf_and_weight in tf_to_weight
+        }
