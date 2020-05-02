5d555e9092fac58a1847c2c30a676170a4282c2d
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-05-02 23:42:25 +0200

    update TV token and webhook management

--- Services/Services_bases/trading_view_service/trading_view_service.py
+++ Services/Services_bases/trading_view_service/trading_view_service.py
@@ -14,19 +14,19 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 import hashlib
+import uuid
 
-from octobot_services.constants import CONFIG_TRADING_VIEW, CONFIG_REQUIRE_TRADING_VIEW_TOKEN, CONFIG_CATEGORY_SERVICES
+from octobot_services.constants import CONFIG_TRADING_VIEW, CONFIG_REQUIRE_TRADING_VIEW_TOKEN, CONFIG_CATEGORY_SERVICES, \
+    CONFIG_TRADING_VIEW_TOKEN
 from octobot_services.services.abstract_service import AbstractService
 
 
 class TradingViewService(AbstractService):
     def __init__(self):
         super().__init__()
-        self.requires_token = False
-        self.chat_id = None
-        self.telegram_updater = None
-        self.users = []
-        self.text_chat_dispatcher = {}
+        self.requires_token = None
+        self.token = None
+        self._webhook_url = None
 
     @staticmethod
     def is_setup_correctly(config):
@@ -40,18 +40,28 @@ class TradingViewService(AbstractService):
         return True
 
     def get_required_config(self):
-        return [CONFIG_REQUIRE_TRADING_VIEW_TOKEN]
+        return [CONFIG_REQUIRE_TRADING_VIEW_TOKEN, CONFIG_TRADING_VIEW_TOKEN]
 
     def get_fields_description(self):
         return {
-            CONFIG_REQUIRE_TRADING_VIEW_TOKEN: "When enabled the Trading View webhook will require a valid token to "
-                                               "process a signal. This token is printed in the terminal at startup."
+            CONFIG_REQUIRE_TRADING_VIEW_TOKEN: "When enabled the Trading View webhook will require your "
+                                               "tradingview.com token to process any signal.",
+            CONFIG_TRADING_VIEW_TOKEN: "Your personal unique tradingview.com token. Can be used to ensure only your "
+                                       "Trading View signals are triggering your OctoBot in case someone else get "
+                                       "your webhook link. You can change it at any moment but remember to change it "
+                                       "on your tradingview.com signal account as well."
         }
 
     def get_default_value(self):
         return {
-            CONFIG_REQUIRE_TRADING_VIEW_TOKEN: False
+            CONFIG_REQUIRE_TRADING_VIEW_TOKEN: False,
+            CONFIG_TRADING_VIEW_TOKEN: self.get_security_token(uuid.uuid4().hex)
         }
+
+    def get_read_only_info(self):
+        return {
+            "Webhook url:": self._webhook_url
+        } if self._webhook_url else {}
 
     @classmethod
     def get_help_page(cls) -> str:
@@ -67,8 +77,18 @@ class TradingViewService(AbstractService):
         try:
             self.requires_token = \
                 self.config[CONFIG_CATEGORY_SERVICES][CONFIG_TRADING_VIEW][CONFIG_REQUIRE_TRADING_VIEW_TOKEN]
+            self.token = self.config[CONFIG_CATEGORY_SERVICES][CONFIG_TRADING_VIEW][CONFIG_TRADING_VIEW_TOKEN]
         except KeyError:
-            self.requires_token = self.get_default_value()[CONFIG_REQUIRE_TRADING_VIEW_TOKEN]
+            if self.requires_token is None:
+                self.requires_token = self.get_default_value()[CONFIG_REQUIRE_TRADING_VIEW_TOKEN]
+            if self.token is None:
+                self.token = self.get_default_value()[CONFIG_TRADING_VIEW_TOKEN]
+            # save new values into config file
+            updated_config = {
+                CONFIG_REQUIRE_TRADING_VIEW_TOKEN: self.requires_token,
+                CONFIG_TRADING_VIEW_TOKEN: self.token
+            }
+            self.save_service_config(CONFIG_TRADING_VIEW, updated_config)
 
     @staticmethod
     def get_security_token(pin_code):
@@ -79,6 +99,9 @@ class TradingViewService(AbstractService):
         """
         token = hashlib.sha224(pin_code.encode('utf-8'))
         return token.hexdigest()
+
+    def register_webhook_url(self, webhook_url):
+        self._webhook_url = webhook_url
 
     def get_successful_startup_message(self):
         return "", True
