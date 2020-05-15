66cb98951ecd27d5e9429a48fa93b6e4bf8eee6f
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-05-15 18:56:56 +0200

    update tentacles packages management

--- Services/Interfaces/web_interface/models/tentacles.py
+++ Services/Interfaces/web_interface/models/tentacles.py
@@ -13,57 +13,59 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
-from octobot.constants import DEFAULT_TENTACLES_URL, DEFAULT_TENTACLES_PACKAGE_NAME
+from octobot.constants import DEFAULT_TENTACLES_URL
 from octobot_commons.logging.logging_util import get_logger
 from octobot_services.interfaces.util.bot import get_bot_api
 from octobot_services.interfaces.util.util import run_in_bot_main_loop
-from octobot_tentacles_manager.api.configurator import get_user_tentacles_packages
+from octobot_tentacles_manager.api.configurator import get_registered_tentacle_packages
 from octobot_tentacles_manager.api.inspector import get_installed_tentacles_modules
 from octobot_tentacles_manager.api.installer import install_all_tentacles
 from octobot_tentacles_manager.api.uninstaller import uninstall_all_tentacles, uninstall_tentacles
 from octobot_tentacles_manager.api.updater import update_all_tentacles, update_tentacles
+from octobot_tentacles_manager.constants import UNKNOWN_TENTACLES_PACKAGE_LOCATION
 
 logger = get_logger("TentaclesModel")
 
 
 def get_tentacles_packages():
-    packages = {
-        DEFAULT_TENTACLES_URL: DEFAULT_TENTACLES_PACKAGE_NAME
-    }
-    for tentacle_package, package_name in get_user_tentacles_packages(get_bot_api().get_edited_tentacles_config()):
-        packages[tentacle_package] = package_name
-    return packages
-
-
-def register_and_install(path_or_url):
-    # TODO or remove if irrelevant in 0.4
-    return False
+    return get_registered_tentacle_packages(get_bot_api().get_edited_tentacles_config())
 
 
 def call_tentacle_manager(coro, *args, **kwargs):
     return run_in_bot_main_loop(coro(*args, **kwargs)) == 0
 
 
-def install_packages():
-    if call_tentacle_manager(install_all_tentacles,
-                             DEFAULT_TENTACLES_URL,
-                             aiohttp_session=get_bot_api().get_aiohttp_session()):
-        return "Tentacles installed"
-    else:
-        return False
+def install_packages(path_or_url=None):
+    message = "Tentacles installed"
+    for package_url in [path_or_url] if path_or_url else \
+            get_registered_tentacle_packages(get_bot_api().get_edited_tentacles_config()).values():
+        if not package_url == UNKNOWN_TENTACLES_PACKAGE_LOCATION:
+            if not call_tentacle_manager(install_all_tentacles,
+                                         package_url,
+                                         setup_config=get_bot_api().get_edited_tentacles_config(),
+                                         aiohttp_session=get_bot_api().get_aiohttp_session()):
+                return False
+        else:
+            message = "Tentacles installed however it is impossible to re-install tentacles with unknown package origin"
+    return message
 
 
 def update_packages():
-    if call_tentacle_manager(update_all_tentacles,
-                             DEFAULT_TENTACLES_URL,
-                             aiohttp_session=get_bot_api().get_aiohttp_session()):
-        return "Tentacles updated"
-    else:
-        return False
+    message = "Tentacles updated"
+    for package_url in get_registered_tentacle_packages(get_bot_api().get_edited_tentacles_config()).values():
+        if not package_url == UNKNOWN_TENTACLES_PACKAGE_LOCATION:
+            if not call_tentacle_manager(update_all_tentacles,
+                                         package_url,
+                                         aiohttp_session=get_bot_api().get_aiohttp_session()):
+                return False
+        else:
+            message = "Tentacles updated however it is impossible to update tentacles with unknown package origin"
+    return message
 
 
 def reset_packages():
     if call_tentacle_manager(uninstall_all_tentacles,
+                             setup_config=get_bot_api().get_edited_tentacles_config(),
                              use_confirm_prompt=False):
         return "Reset successful"
     else:
