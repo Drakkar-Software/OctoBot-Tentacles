c8bc7719ac41726bcd41df493c6589ade0ad65b0
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-05-16 19:10:10 +0200

    update strategy tests values according to updated mark price and profitability management

--- Evaluator/Strategies/mixed_strategies_evaluator/tests/test_technical_analysis_strategy_evaluator.py
+++ Evaluator/Strategies/mixed_strategies_evaluator/tests/test_technical_analysis_strategy_evaluator.py
@@ -39,41 +39,41 @@ class TechnicalAnalysisStrategyEvaluatorTest(AbstractStrategyTest):
     """
 
     async def test_default_run(self):
-        # market: -12.052505966587105
-        await self.run_test_default_run(-6.213)
+        # market: -12.643224022125636
+        await self.run_test_default_run(-5.988)
 
     async def test_slow_downtrend(self):
-        # market: -12.052505966587105
-        # market: -15.195702225633141
-        # market: -29.12366137549725
-        # market: -32.110091743119256
-        await self.run_test_slow_downtrend(-6.213, -4.773, -10.92, -5.737)
+        # market: -12.643224022125636
+        # market: -16.39871382636656
+        # market: -28.59375
+        # market: -31.162790697674424
+        await self.run_test_slow_downtrend(-5.988, -4.464, -10.424, -8.468)
 
     async def test_sharp_downtrend(self):
-        # market: -26.07183938094741
-        # market: -32.1654501216545
-        await self.run_test_sharp_downtrend(-15.471, -19.481)
+        # market: -26.019620667102686
+        # market: -31.958511287370357
+        await self.run_test_sharp_downtrend(-15.781, -20.087)
 
     async def test_flat_markets(self):
-        # market: -10.560669456066947
-        # market: -3.401191658391241
-        # market: -5.7854560064282765
-        # market: -8.067940552016978
-        await self.run_test_flat_markets(-1.538, 1.939, -7.731, 5.081)
+        # market: -10.814419225634182
+        # market: -3.2570860268523063
+        # market: -5.367231638418076
+        # market: -7.921318447634235
+        await self.run_test_flat_markets(-2.022, 1.591, -6.829, -0.749)
 
     async def test_slow_uptrend(self):
-        # market: 17.203948364436457
-        # market: 16.19613670133728
-        await self.run_test_slow_uptrend(4.707, 13.005)
+        # market: 16.65469559146318
+        # market: 16.31135349529005
+        await self.run_test_slow_uptrend(4.128, 12.568)
 
     async def test_sharp_uptrend(self):
-        # market: 30.881852230166828
-        # market: 12.28597871355852
-        await self.run_test_sharp_uptrend(13.97, 10.842)
+        # market: 30.659415363698173
+        # market: 11.435132032146939
+        await self.run_test_sharp_uptrend(11.552, 10.307)
 
     async def test_up_then_down(self):
-        # market: -6.040105108015155
-        await self.run_test_up_then_down(-3.998)
+        # market: -6.684034772021235
+        await self.run_test_up_then_down(-2.285)
 
 
 async def test_default_run(strategy_tester):
