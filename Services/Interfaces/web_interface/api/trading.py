7cab1fcfe055a5d96ba9da69df34dbf19f17c813
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-05-18 14:01:37 +0200

    add web interface browsing tests

--- Services/Interfaces/web_interface/api/trading.py
+++ Services/Interfaces/web_interface/api/trading.py
@@ -13,11 +13,10 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
-
 import json
 from flask import request, jsonify
 
-from octobot_services.interfaces.util.order import get_open_orders, cancel_orders
+from octobot_services.interfaces.util.order import cancel_orders, get_all_open_orders
 from octobot_services.interfaces.util.trader import force_real_traders_refresh
 from . import api
 from tentacles.Services.Interfaces.web_interface.util.flask_util import get_rest_reply
@@ -26,7 +25,7 @@ from tentacles.Services.Interfaces.web_interface.util.flask_util import get_rest
 @api.route("/orders", methods=['GET', 'POST'])
 def orders():
     if request.method == 'GET':
-        real_open_orders, simulated_open_orders = get_open_orders()
+        real_open_orders, simulated_open_orders = get_all_open_orders()
 
         return json.dumps({"real_open_orders": real_open_orders, "simulated_open_orders": simulated_open_orders})
     elif request.method == "POST":
