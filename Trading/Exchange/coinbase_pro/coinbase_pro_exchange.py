c6d0a9c6fdeb64657deede032e1bd6f7581f2fe4
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-10-22 21:50:34 +0200

    [Exchanges] udpate coinbase pro market_status handling

--- Trading/Exchange/coinbase_pro/coinbase_pro_exchange.py
+++ Trading/Exchange/coinbase_pro/coinbase_pro_exchange.py
@@ -13,6 +13,11 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
+import copy
+import math
+import ccxt
+
+import octobot_trading.errors
 import octobot_trading.enums as trading_enums
 import octobot_trading.exchanges as exchanges
 
@@ -25,6 +30,19 @@ class CoinbasePro(exchanges.SpotCCXTExchange):
     @classmethod
     def is_supporting_exchange(cls, exchange_candidate_name) -> bool:
         return cls.get_name() == exchange_candidate_name
+
+    def get_market_status(self, symbol, price_example=None, with_fixer=True):
+        try:
+            # on coinbasepro, precision is a decimal instead of a number of digits
+            market_status = self._fix_market_status(copy.deepcopy(self.client.market(symbol)))
+            if with_fixer:
+                market_status = exchanges.ExchangeMarketStatusFixer(market_status, price_example).market_status
+            return market_status
+        except ccxt.NotSupported:
+            raise octobot_trading.errors.NotSupported
+        except Exception as e:
+            self.logger.error(f"Fail to get market status of {symbol}: {e}")
+            return {}
 
     async def cancel_order(self, order_id, symbol=None, params=None):
         self.logger.debug(f"Post cancel for order with id={order_id}")
@@ -54,3 +72,17 @@ class CoinbasePro(exchanges.SpotCCXTExchange):
                 if trade["takerOrMaker"] == trading_enums.ExchangeConstantsMarketPropertyColumns.TAKER.value \
                 else trading_enums.TradeOrderType.LIMIT.value
         return trades
+
+    def _fix_market_status(self, market_status):
+        market_status[trading_enums.ExchangeConstantsMarketStatusColumns.PRECISION.value][
+            trading_enums.ExchangeConstantsMarketStatusColumns.PRECISION_AMOUNT.value] = self._get_digits_count(
+            market_status[trading_enums.ExchangeConstantsMarketStatusColumns.PRECISION.value][
+                trading_enums.ExchangeConstantsMarketStatusColumns.PRECISION_AMOUNT.value])
+        market_status[trading_enums.ExchangeConstantsMarketStatusColumns.PRECISION.value][
+            trading_enums.ExchangeConstantsMarketStatusColumns.PRECISION_PRICE.value] = self._get_digits_count(
+            market_status[trading_enums.ExchangeConstantsMarketStatusColumns.PRECISION.value][
+                trading_enums.ExchangeConstantsMarketStatusColumns.PRECISION_PRICE.value])
+        return market_status
+
+    def _get_digits_count(self, value):
+        return round(abs(math.log(value, 10)))
