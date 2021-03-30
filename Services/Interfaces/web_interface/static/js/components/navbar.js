1575d1661d0924eb606d0c5db1fb9b41114bb9b5
herklos <herklos.dev@protonmail.com>
2021-03-30 21:00:50 +0200

    [WebInterface] Add js request

--- Services/Interfaces/web_interface/static/js/components/navbar.js
+++ Services/Interfaces/web_interface/static/js/components/navbar.js
@@ -16,8 +16,40 @@
  * License along with this library.
  */
 
+function trigger_trader_state(element) {
+    let updated_config = {};
+    const update_url = element.attr(update_url_attr);
+    const config_key = element.attr(config_key_attr);
+    const config_type = element.attr(config_type_attr);
+    const set_to_activated = element.attr(current_value_attr).toLowerCase() === "true";
+
+    if (config_key === "trader_enabled") {
+        updated_config = {
+            [config_type]: {
+                "trader_enabled": set_to_activated,
+                "trader-simulator_enabled": !set_to_activated,
+            }
+        }
+    } else {
+        updated_config = {
+            [config_type]: {
+                "trader_enabled": !set_to_activated,
+                "trader-simulator_enabled": set_to_activated,
+            }
+        }
+    }
+
+    updated_config["restart_after_save"] = true;
+
+    // send update
+    send_and_interpret_bot_update(updated_config, update_url, null);
+}
+
 $(document).ready(function() {
     $("#switchTradingState").click(function(){
         showModalIfAny($("#tradingSwitchModal"))
+    });
+    $(".trading-mode-switch-button").click(function(){
+        trigger_trader_state($(this))
     });
 });
