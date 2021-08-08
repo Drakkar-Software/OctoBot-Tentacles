ca95bda44d7f7d31c58bc4dc192f07cdb0c2d37a
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-08-08 15:42:30 +0200

    [Exchanges] do not display valid auth with invalid creds

--- Services/Interfaces/web_interface/static/js/components/configuration.js
+++ Services/Interfaces/web_interface/static/js/components/configuration.js
@@ -645,27 +645,45 @@ function update_exchanges_details(exchangeCard, exchangeData){
     const warnDetails = $(exchangeCard.find("[data-role=account-warning-details]"));
     const partnersDocs = $(exchangeCard.find("[data-role=account-partners-docs]"));
     const supportingIcon = $(exchangeCard.find("[data-role=supporting-account]"));
+    const supporterAccountWarn = $(exchangeCard.find("[data-role=supporter-account]"))
     partnersDocs.addClass(hidden_class);
     if(exchangeData["supporting"]){
-        if(exchangeData["supporter_account"] || exchangeData["compatible"]){
+        supporterAccountWarn.removeClass(hidden_class);
+        if (exchangeData["supporter_account"]){
             warnIcon.addClass(hidden_class);
             warnDetailsWrapper.addClass(hidden_class);
-            validIcon.addClass(hidden_class);
-            unloggedSupportingIcon.addClass(hidden_class);
             supportingIcon.removeClass(hidden_class);
+            validIcon.addClass(hidden_class);
+            supporterAccountWarn.addClass(hidden_class)
+        }
+        if(exchangeData["compatible"]){
+            warnIcon.addClass(hidden_class);
+            warnDetailsWrapper.addClass(hidden_class);
+            validIcon.removeClass(hidden_class);
+            unloggedSupportingIcon.addClass(hidden_class);
         }else{
             if(exchangeData["configured"]) {
-                warnIcon.addClass(hidden_class);
-                supportingIcon.addClass(hidden_class);
-                unloggedSupportingIcon.addClass(hidden_class);
-                warnDetailsWrapper.removeClass(hidden_class);
-                if (exchangeData["error_message"].includes("create a new")) {
-                    partnersDocs.removeClass(hidden_class);
-                    warnDetails.text(exchangeData["error_message"]);
-                    warnIcon.removeClass(hidden_class);
+                if(exchangeData["supporter_account"]) {
+                    // supporter account have websocket access
+                    warnIcon.addClass(hidden_class);
+                    if (!exchangeData["error_message"].includes("create a new")) {
+                        warnDetailsWrapper.removeClass(hidden_class);
+                        warnDetails.text(exchangeData["error_message"]);
+                    }
                 }else{
-                    warnDetails.text(exchangeData["error_message"]);
-                    unloggedSupportingIcon.removeClass(hidden_class);
+                    warnIcon.addClass(hidden_class);
+                    supportingIcon.addClass(hidden_class);
+                    unloggedSupportingIcon.addClass(hidden_class);
+                    warnDetailsWrapper.removeClass(hidden_class);
+                    if (exchangeData["error_message"].includes("create a new")) {
+                        partnersDocs.removeClass(hidden_class);
+                        warnDetails.text(exchangeData["error_message"]);
+                        warnIcon.removeClass(hidden_class);
+                    }else{
+                        warnDetails.text(exchangeData["error_message"]);
+                        unloggedSupportingIcon.removeClass(hidden_class);
+                        supporterAccountWarn.addClass(hidden_class);
+                    }
                 }
             } else {
                 supportingIcon.addClass(hidden_class);
@@ -676,6 +694,7 @@ function update_exchanges_details(exchangeCard, exchangeData){
             }
         }
     }else {
+        supporterAccountWarn.addClass(hidden_class);
         warnIcon.addClass(hidden_class);
         supportingIcon.addClass(hidden_class);
         if(exchangeData["compatible"]){
