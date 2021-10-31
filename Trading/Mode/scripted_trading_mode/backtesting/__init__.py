6d54a36fc1e1d0abfe3156e5bd0358b1836eb1d4
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-10-31 02:03:28 +0200

    [ScriptedTradingMode] improve UI and merge scripts

---
+++ Trading/Mode/scripted_trading_mode/backtesting/__init__.py
@@ -0,0 +1,1 @@
+from .example_backtesting_script import backtest_test_script
---
+++ Trading/Mode/scripted_trading_mode/trading/__init__.py
@@ -0,0 +1,4 @@
+# import tentacles.Trading.Mode.scripted_trading_mode.trading.example_trading_script
+# from tentacles.Trading.Mode.scripted_trading_mode.trading.example_trading_script import script
+
+from .example_trading_script import *
