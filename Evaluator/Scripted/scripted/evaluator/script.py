23db1c385a3f2cb9c732805b3da1ecee5d4b20bc
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-11-21 00:20:04 +0100

    [Evaluators] add scripted evaluator and blank strategy

---
+++ Evaluator/Scripted/scripted/evaluator/script.py
@@ -0,0 +1,69 @@
+import octobot_commons.constants as commons_constants
+import octobot_commons.enums as commons_enums
+from octobot_trading.modes.scripting_library import *
+
+
+async def script(ctx: Context):
+    set_script_name(ctx, "EMA Trend")
+
+    ema_length = await user_input(ctx, "EMA length", "int", 15)
+    data_source = await user_input(ctx, "data source", "options", "close", options=["open", "high", "low", "close"])
+    # available_timeframes = ["1h", "4h"]  # todo keyword for list of available timeframes
+    # ctx.time_frame = await user_input(ctx, "Time Frame", "options", "1h", options=available_timeframes) # todo why do we need ctx.timeframe?
+
+    ema_title = str(ema_length) + " EMA"
+    ema_rising_title = str(ema_length) + " EMA is rising"
+    ema_falling_title = str(ema_length) + " EMA is falling"
+    time_frame_seconds = commons_enums.TimeFramesMinutes[commons_enums.TimeFrames(ctx.time_frame)] * commons_constants.MINUTE_TO_SECONDS
+    last_timestamp = ctx.trigger_cache_timestamp - time_frame_seconds
+
+    ema_value, is_ema_value_missing = await ctx.get_cached_value(ema_title)
+    ema_is_rising, ema_is_rising_missing = await ctx.get_cached_value(ema_rising_title)
+    ema_is_falling, ema_is_falling_missing = await ctx.get_cached_value(ema_falling_title)
+    previous_ema_value, is_previous_ema_value_missing = await ctx.get_cached_value(ema_title, cache_key=last_timestamp)
+    super_complicated_value, is_complicated_value_missing = await ctx.get_cached_value()
+
+    if is_complicated_value_missing or is_ema_value_missing or is_previous_ema_value_missing:
+        pair = ctx.traded_pair
+        if is_ema_value_missing or is_previous_ema_value_missing:
+            candle_source = None
+            if data_source == "close":
+                candle_source = Close(ctx, pair, ctx.time_frame, ema_length + 1)
+            elif data_source == "open":
+                candle_source = Open(ctx, pair, ctx.time_frame, ema_length + 1)
+            elif data_source == "high":
+                candle_source = High(ctx, pair, ctx.time_frame, ema_length + 1)
+            elif data_source == "low":
+                candle_source = Low(ctx, pair, ctx.time_frame)
+
+            if len(candle_source) > ema_length:  # can we remove that somehow?
+                ema_data = ti.ema(candle_source, ema_length)
+                ema_value = ema_data[-1]
+                previous_ema_value = ema_data[-2]
+                await ctx.set_cached_value(ema_value, ema_title)
+                await ctx.set_cached_value(previous_ema_value, ema_title, cache_key=last_timestamp)
+        if ema_is_rising_missing and ema_value and previous_ema_value:
+            ema_is_rising = previous_ema_value < ema_value
+            plot_location_low = Low(ctx, pair, ctx.time_frame)[-1] - 20
+            await ctx.set_cached_value(ema_is_rising, ema_rising_title, y=plot_location_low)
+        if ema_is_falling_missing and ema_value and previous_ema_value:
+            ema_is_falling = previous_ema_value > ema_value
+            plot_location_high = High(ctx, pair, ctx.time_frame)[-1] + 20
+            await ctx.set_cached_value(ema_is_falling, ema_falling_title, y=plot_location_high)
+
+        super_complicated_value = 0.5 if ema_is_rising else -0.5
+
+    await plot(ctx, ema_title, cache_value=ema_title, chart=trading_enums.PlotCharts.MAIN_CHART.value)
+    await plot(ctx, ema_rising_title, condition=True, cache_value=ema_rising_title,
+               chart=trading_enums.PlotCharts.MAIN_CHART.value, kind="markers")
+    await plot(ctx, ema_falling_title, condition=True, cache_value=ema_falling_title,
+               chart=trading_enums.PlotCharts.MAIN_CHART.value, kind="markers")
+
+    # await plot(ctx, ema_title, t, ema_data, chart=trading_enums.PlotCharts.MAIN_CHART.value)
+    # await plot(ctx, ema_rising_title, condition=ema_is_rising, y=plot_location_low,
+    #            chart=trading_enums.PlotCharts.MAIN_CHART.value, kind="markers")
+    # await plot(ctx, ema_falling_title, condition=ema_is_falling, y=plot_location_high,
+    #            chart=trading_enums.PlotCharts.MAIN_CHART.value, kind="markers")
+
+    # TODO: strategy trigger
+    return super_complicated_value
---
+++ Evaluator/Strategies/blank_strategy_evaluator/blank_strategy.py
@@ -0,0 +1,31 @@
+#  Drakkar-Software OctoBot-Tentacles
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+import octobot_evaluators.evaluators as evaluators
+
+
+class BlankStrategyEvaluator(evaluators.StrategyEvaluator):
+    async def matrix_callback(self,
+                              matrix_id,
+                              evaluator_name,
+                              evaluator_type,
+                              eval_note,
+                              eval_note_type,
+                              exchange_name,
+                              cryptocurrency,
+                              symbol,
+                              time_frame):
+        self.eval_note = eval_note
+        await self.strategy_completed(cryptocurrency, symbol)
