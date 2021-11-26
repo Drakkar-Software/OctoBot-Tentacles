bf8a92014f1e3e2d8d185aaad7f060c5859808c4
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-11-26 23:52:18 +0100

    [ScriptedTrading] update backtesting_script

--- Trading/Mode/scripted_trading_mode/backtesting/backtesting_script.py
+++ Trading/Mode/scripted_trading_mode/backtesting/backtesting_script.py
@@ -5,14 +5,14 @@ import time
 from octobot_trading.modes.scripting_library import *
 
 
-async def script(ctx, run_id):
+async def script(ctx):
     drawing = DisplayedElements()
-    async with DBReader.database(get_backtesting_db(ctx, run_id)) as reader:
+    async with ctx.run_data() as run_data:
         with drawing.part("backtesting-chart") as part:
-            await plot_historical_portfolio_value(reader, part)
-            await plot_historical_pnl_value(reader, part, x_as_trade_count=False, own_yaxis=True)
+            await plot_historical_portfolio_value(run_data, part)
+            await plot_historical_pnl_value(run_data, part, x_as_trade_count=False, own_yaxis=True)
         with drawing.part("list-of-trades-part", "table") as part:
-            await plot_table(reader, part, "SMA 1")
-            await plot_table(reader, part, "SMA 2")
-            await plot_trades(reader, part)
+            await plot_table(run_data, part, "SMA 1")
+            await plot_table(run_data, part, "SMA 2")
+            await plot_trades(run_data, part)
     return drawing
