2f649272248964023f38fc8c1755823e75d42353
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-12-19 18:45:54 +0100

    [ScriptedTradingMode] add plot_cumulative_historical_pnl_value

--- Trading/Mode/scripted_trading_mode/script_templates/backtesting_scripts/backtesting_script.py
+++ Trading/Mode/scripted_trading_mode/script_templates/backtesting_scripts/backtesting_script.py
@@ -1,7 +1,3 @@
-from tentacles.Evaluator.TA import *
-import octobot_trading.api as api
-import octobot_commons.enums as commons_enums
-import time
 from octobot_trading.modes.scripting_library import *
 
 
@@ -11,16 +7,15 @@ async def script(ctx):
         with drawing.part("backtesting-run-overview") as part:
             await plot_historical_portfolio_value(run_data, part)
             await plot_historical_pnl_value(run_data, part, x_as_trade_count=False, own_yaxis=True)
+            await plot_cumulative_historical_pnl_value(run_data, part, x_as_trade_count=False, own_yaxis=True)
         with drawing.part("backtesting-details", "value") as part:
-            pnl = await backtesting_data(run_data, "p&l")
-            pnl_p = await backtesting_data(run_data, "p&l%")
-            all_orders = await backtesting_data(run_data, "orders")
-            await display(part, "% P&L", pnl_p)
-            await display(part, "P&L", pnl)
-            order_count = len(all_orders) if all_orders else 0
-            await display(part, "Orders", order_count)
-            paid_fees = (sum(order["fees_amount"] for order in all_orders) ) if all_orders else 0
-            await display(part, "paid fees", paid_fees)
+            pnl = await backtesting_data(run_data, BacktestingMetadata.GAINS)
+            pnl_p = await backtesting_data(run_data, BacktestingMetadata.PERCENT_GAINS)
+            all_trades = await backtesting_data(run_data, "all_trades") or []
+            await display(part, "% gains", pnl_p)
+            await display(part, "Gains", pnl)
+            await display(part, "Trades", len(all_trades))
+            await display(part, "paid fees", sum(order["fees_amount"] for order in all_trades))
         with drawing.part("list-of-trades-part", "table") as part:
             await plot_table(run_data, part, "SMA 1")
             await plot_table(run_data, part, "SMA 2")
