a2c2d49782ad5be4b8a6c721dbeb83741552e57a
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-04-23 23:30:07 +0200

    [RemoteTradingSignalsTradingMode] fixes and add tests

---
+++ Trading/Mode/remote_trading_signals_trading_mode/tests/__init__.py
@@ -0,0 +1,178 @@
+#  Drakkar-Software OctoBot-Tentacles
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+import os.path
+import mock
+import pytest
+
+import octobot_backtesting.api as backtesting_api
+import async_channel.util as channel_util
+import octobot_commons.tests.test_config as test_config
+import octobot_commons.constants as commons_constants
+import octobot_commons.asyncio_tools as asyncio_tools
+import octobot_trading.api as trading_api
+import octobot_trading.exchange_channel as exchanges_channel
+import octobot_trading.exchanges as exchanges
+import octobot_trading.personal_data as trading_personal_data
+import octobot_trading.signals as trading_signals
+import octobot_trading.enums as trading_enums
+import tentacles.Trading.Mode as modes
+import tests.test_utils.config as test_utils_config
+from tentacles.Trading.Mode.remote_trading_signals_trading_mode.remote_trading_signals_trading import \
+    RemoteTradingSignalsTradingMode
+import octobot_tentacles_manager.api as tentacles_manager_api
+
+
+@pytest.fixture
+async def exchange(exchange_name="binance", backtesting=None, symbol="BTC/USDT"):
+    tentacles_manager_api.reload_tentacle_info()
+    exchange_manager = None
+    try:
+        config = test_config.load_test_config()
+        config[commons_constants.CONFIG_SIMULATOR][commons_constants.CONFIG_STARTING_PORTFOLIO]["USDT"] = 2000
+        exchange_manager = exchanges.ExchangeManager(config, exchange_name)
+        exchange_manager.tentacles_setup_config = test_utils_config.get_tentacles_setup_config()
+
+        # use backtesting not to spam exchanges apis
+        exchange_manager.is_simulated = True
+        exchange_manager.is_backtesting = True
+        backtesting = backtesting or await backtesting_api.initialize_backtesting(
+            config,
+            exchange_ids=[exchange_manager.id],
+            matrix_id=None,
+            data_files=[os.path.join(test_config.TEST_CONFIG_FOLDER,
+                                     "AbstractExchangeHistoryCollector_1586017993.616272.data")])
+
+        exchange_manager.exchange = exchanges.ExchangeSimulator(exchange_manager.config,
+                                                                exchange_manager,
+                                                                backtesting)
+        await exchange_manager.exchange.initialize()
+        for exchange_channel_class_type in [exchanges_channel.ExchangeChannel,
+                                            exchanges_channel.TimeFrameExchangeChannel]:
+            await channel_util.create_all_subclasses_channel(exchange_channel_class_type, exchanges_channel.set_chan,
+                                                             exchange_manager=exchange_manager)
+
+        trader = exchanges.TraderSimulator(config, exchange_manager)
+        await trader.initialize()
+
+        mode = modes.RemoteTradingSignalsTradingMode(config, exchange_manager)
+        mode.symbol = None if mode.get_is_symbol_wildcard() else symbol
+        # avoid error when trying to connect to server signals
+        with mock.patch.object(RemoteTradingSignalsTradingMode, "_subscribe_to_signal_feed",
+                               new=mock.AsyncMock(return_value=[])) \
+                as _subscribe_to_signal_feed_mock:
+            await trading_signals.create_remote_trading_signal_channel_if_missing(
+                exchange_manager
+            )
+            await mode.initialize()
+            # add mode to exchange manager so that it can be stopped and freed from memory
+            exchange_manager.trading_modes.append(mode)
+
+            # set BTC/USDT price at 1000 USDT
+            trading_api.force_set_mark_price(exchange_manager, symbol, 1000)
+            # let trading modes start
+            await asyncio_tools.wait_asyncio_next_cycle()
+            _subscribe_to_signal_feed_mock.assert_called_once()
+        yield mode.producers[0], mode.consumers[0], exchange_manager
+    finally:
+        if exchange_manager is not None:
+            for importer in backtesting_api.get_importers(exchange_manager.exchange.backtesting):
+                await backtesting_api.stop_importer(importer)
+            if exchange_manager.exchange.backtesting.time_updater is not None:
+                await exchange_manager.exchange.backtesting.stop()
+            await exchange_manager.stop()
+
+
+@pytest.fixture
+def mocked_signal():
+    orders = [
+        {
+            trading_enums.TradingSignalOrdersAttrs.ACTION.value: trading_enums.TradingSignalOrdersActions.CREATE.value,
+            trading_enums.TradingSignalOrdersAttrs.SIDE.value: trading_enums.TradeOrderSide.SELL.value,
+            trading_enums.TradingSignalOrdersAttrs.TYPE.value: trading_enums.TraderOrderType.STOP_LOSS.value,
+            trading_enums.TradingSignalOrdersAttrs.QUANTITY.value: 0.004,
+            trading_enums.TradingSignalOrdersAttrs.TARGET_AMOUNT.value: "5.356892%",
+            trading_enums.TradingSignalOrdersAttrs.TARGET_POSITION.value: 0,
+            trading_enums.TradingSignalOrdersAttrs.UPDATED_TARGET_AMOUNT.value: 0,
+            trading_enums.TradingSignalOrdersAttrs.UPDATED_TARGET_POSITION.value: 0,
+            trading_enums.TradingSignalOrdersAttrs.LIMIT_PRICE.value: 9990.0,
+            trading_enums.TradingSignalOrdersAttrs.UPDATED_LIMIT_PRICE.value: 0.0,
+            trading_enums.TradingSignalOrdersAttrs.STOP_PRICE.value: 0.0,
+            trading_enums.TradingSignalOrdersAttrs.UPDATED_STOP_PRICE.value: 0.0,
+            trading_enums.TradingSignalOrdersAttrs.CURRENT_PRICE.value: 1000.69,
+            trading_enums.TradingSignalOrdersAttrs.UPDATED_CURRENT_PRICE.value: 0.0,
+            trading_enums.TradingSignalOrdersAttrs.REDUCE_ONLY.value: True,
+            trading_enums.TradingSignalOrdersAttrs.POST_ONLY.value: False,
+            trading_enums.TradingSignalOrdersAttrs.GROUP_ID.value: "46a0b2de-5b8f-4a39-89a0-137504f83dfc",
+            trading_enums.TradingSignalOrdersAttrs.GROUP_TYPE.value:
+                trading_personal_data.BalancedTakeProfitAndStopOrderGroup.__name__,
+            trading_enums.TradingSignalOrdersAttrs.TAG.value: "managed_order long exit (id: 143968020)",
+            trading_enums.TradingSignalOrdersAttrs.SHARED_SIGNAL_ORDER_ID.value: "5ad2a999-5ac2-47f0-9b69-c75a36f3858a",
+            trading_enums.TradingSignalOrdersAttrs.BUNDLED_WITH.value: "adc24701-573b-40dd-b6c9-3666cd22f33e",
+            trading_enums.TradingSignalOrdersAttrs.CHAINED_TO.value: "adc24701-573b-40dd-b6c9-3666cd22f33e"
+        },
+        {
+            trading_enums.TradingSignalOrdersAttrs.ACTION.value: trading_enums.TradingSignalOrdersActions.CREATE.value,
+            trading_enums.TradingSignalOrdersAttrs.SIDE.value: trading_enums.TradeOrderSide.BUY.value,
+            trading_enums.TradingSignalOrdersAttrs.TYPE.value: trading_enums.TraderOrderType.BUY_MARKET.value,
+            trading_enums.TradingSignalOrdersAttrs.QUANTITY.value: 0.004,
+            trading_enums.TradingSignalOrdersAttrs.TARGET_AMOUNT.value: "5.356892%",
+            trading_enums.TradingSignalOrdersAttrs.TARGET_POSITION.value: None,
+            trading_enums.TradingSignalOrdersAttrs.UPDATED_TARGET_AMOUNT.value: 0,
+            trading_enums.TradingSignalOrdersAttrs.UPDATED_TARGET_POSITION.value: 0,
+            trading_enums.TradingSignalOrdersAttrs.LIMIT_PRICE.value: 1001.69,
+            trading_enums.TradingSignalOrdersAttrs.UPDATED_LIMIT_PRICE.value: 0.0,
+            trading_enums.TradingSignalOrdersAttrs.STOP_PRICE.value: 0.0,
+            trading_enums.TradingSignalOrdersAttrs.UPDATED_STOP_PRICE.value: 0.0,
+            trading_enums.TradingSignalOrdersAttrs.CURRENT_PRICE.value: 1000.69,
+            trading_enums.TradingSignalOrdersAttrs.UPDATED_CURRENT_PRICE.value: 0.0,
+            trading_enums.TradingSignalOrdersAttrs.REDUCE_ONLY.value: False,
+            trading_enums.TradingSignalOrdersAttrs.POST_ONLY.value: False,
+            trading_enums.TradingSignalOrdersAttrs.GROUP_ID.value: None,
+            trading_enums.TradingSignalOrdersAttrs.GROUP_TYPE.value: None,
+            trading_enums.TradingSignalOrdersAttrs.TAG.value: "managed_order long entry (id: 143968020)",
+            trading_enums.TradingSignalOrdersAttrs.SHARED_SIGNAL_ORDER_ID.value: "adc24701-573b-40dd-b6c9-3666cd22f33e",
+            trading_enums.TradingSignalOrdersAttrs.BUNDLED_WITH.value: None,
+            trading_enums.TradingSignalOrdersAttrs.CHAINED_TO.value: None
+        },
+        {
+            trading_enums.TradingSignalOrdersAttrs.ACTION.value: trading_enums.TradingSignalOrdersActions.CREATE.value,
+            trading_enums.TradingSignalOrdersAttrs.SIDE.value: trading_enums.TradeOrderSide.SELL.value,
+            trading_enums.TradingSignalOrdersAttrs.TYPE.value: trading_enums.TraderOrderType.SELL_LIMIT.value,
+            trading_enums.TradingSignalOrdersAttrs.QUANTITY.value: 0.004,
+            trading_enums.TradingSignalOrdersAttrs.TARGET_AMOUNT.value: "5.3574085830652285%",
+            trading_enums.TradingSignalOrdersAttrs.TARGET_POSITION.value: 0,
+            trading_enums.TradingSignalOrdersAttrs.UPDATED_TARGET_AMOUNT.value: 0,
+            trading_enums.TradingSignalOrdersAttrs.UPDATED_TARGET_POSITION.value: 0,
+            trading_enums.TradingSignalOrdersAttrs.LIMIT_PRICE.value: 1010.69,
+            trading_enums.TradingSignalOrdersAttrs.UPDATED_LIMIT_PRICE.value: 0.0,
+            trading_enums.TradingSignalOrdersAttrs.STOP_PRICE.value: 0.0,
+            trading_enums.TradingSignalOrdersAttrs.UPDATED_STOP_PRICE.value: 0.0,
+            trading_enums.TradingSignalOrdersAttrs.CURRENT_PRICE.value: 1000.69,
+            trading_enums.TradingSignalOrdersAttrs.UPDATED_CURRENT_PRICE.value: 0.0,
+            trading_enums.TradingSignalOrdersAttrs.REDUCE_ONLY.value: True,
+            trading_enums.TradingSignalOrdersAttrs.POST_ONLY.value: False,
+            trading_enums.TradingSignalOrdersAttrs.GROUP_ID.value: "46a0b2de-5b8f-4a39-89a0-137504f83dfc",
+            trading_enums.TradingSignalOrdersAttrs.GROUP_TYPE.value:
+                trading_personal_data.BalancedTakeProfitAndStopOrderGroup.__name__,
+            trading_enums.TradingSignalOrdersAttrs.TAG.value: "managed_order long exit (id: 143968020)",
+            trading_enums.TradingSignalOrdersAttrs.SHARED_SIGNAL_ORDER_ID.value: "5705d395-f970-45d9-9ba8-f63da17f17b2",
+            trading_enums.TradingSignalOrdersAttrs.BUNDLED_WITH.value: None,
+            trading_enums.TradingSignalOrdersAttrs.CHAINED_TO.value: "adc24701-573b-40dd-b6c9-3666cd22f33e"
+        }
+    ]
+    return trading_signals.TradingSignal("moonmoon", "bybit", trading_enums.TradingSignalExchangeTypes.SPOT.value,
+                                         "BTC/USDT", None, None, orders, identifier="identifier",
+                                         version="1.0.0")
---
+++ Trading/Mode/remote_trading_signals_trading_mode/tests/test_remote_trading_signals_trading_producer.py
@@ -0,0 +1,53 @@
+#  Drakkar-Software OctoBot-Tentacles
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+import pytest
+import mock
+
+import octobot_commons.constants as common_constants
+import octobot_trading.enums as trading_enums
+from tentacles.Trading.Mode.remote_trading_signals_trading_mode.tests import exchange, mocked_signal
+
+
+# All test coroutines will be treated as marked.
+pytestmark = pytest.mark.asyncio
+
+
+async def test_signal_callback(exchange, mocked_signal):
+    producer, _, _ = exchange
+    with mock.patch.object(producer, "submit_trading_evaluation", new=mock.AsyncMock()) \
+         as submit_trading_evaluation_mock:
+        await producer.signal_callback(mocked_signal)
+        submit_trading_evaluation_mock.assert_called_once_with(
+            cryptocurrency=producer.trading_mode.cryptocurrency,
+            symbol=mocked_signal.symbol,
+            time_frame=None,
+            final_note=common_constants.INIT_EVAL_NOTE,
+            state=trading_enums.EvaluatorStates(mocked_signal.state),
+            data=mocked_signal
+        )
+
+        submit_trading_evaluation_mock.reset_mock()
+        # with incompatible exchange type
+        mocked_signal.exchange_type = trading_enums.TradingSignalExchangeTypes.MARGIN.value
+        await producer.signal_callback(mocked_signal)
+        submit_trading_evaluation_mock.assert_not_called()
+
+        # with invalid state
+        mocked_signal.exchange_type = trading_enums.TradingSignalExchangeTypes.SPOT.value
+        mocked_signal.state = "123"
+        with pytest.raises(ValueError):
+            await producer.signal_callback(mocked_signal)
+        submit_trading_evaluation_mock.assert_not_called()
