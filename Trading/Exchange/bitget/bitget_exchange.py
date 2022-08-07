d364518799e6d156d66e7a8410cfebef36970bb7
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-08-07 23:44:08 +0200

    [Bitget] fix trade fetch and order creation

--- Trading/Exchange/bitget/bitget_exchange.py
+++ Trading/Exchange/bitget/bitget_exchange.py
@@ -49,7 +49,7 @@ class Bitget(exchanges.SpotCCXTExchange):
                            price: decimal.Decimal = None, stop_price: decimal.Decimal = None,
                            side: trading_enums.TradeOrderSide = None, current_price: decimal.Decimal = None,
                            params: dict = None) -> typing.Optional[dict]:
-        convert_quantity = order_type in (trading_enums.TraderOrderType.BUY_MARKET, trading_enums.TraderOrderType.SELL_MARKET)
+        convert_quantity = order_type is trading_enums.TraderOrderType.BUY_MARKET
         if convert_quantity:
             # on Bitget, market orders are in quote currency (YYY in XYZ/YYY)
             if price is None:
@@ -60,11 +60,29 @@ class Bitget(exchanges.SpotCCXTExchange):
                                                    price=price, stop_price=stop_price,
                                                    side=side, current_price=current_price,
                                                    params=params)
+        # ensure order side (issue with current ccxt version)
+        created_order[trading_enums.ExchangeConstantsOrderColumns.SIDE.value] = side.value
         if convert_quantity:
             # convert it back: use FILLED for accuracy
             created_order[trading_enums.ExchangeConstantsOrderColumns.AMOUNT.value] = \
                 created_order[trading_enums.ExchangeConstantsOrderColumns.FILLED.value]
         return created_order
+
+    async def get_my_recent_trades(self, symbol=None, since=None, limit=None, **kwargs):
+        return self._uniformize_trades(await super().get_my_recent_trades(symbol=symbol,
+                                                                          since=since,
+                                                                          limit=limit,
+                                                                          **kwargs))
+
+    def _uniformize_trades(self, trades):
+        for trade in trades:
+            # fees example for paid fees in USDT:
+            # {'code': 'USDT', 'cost': -0.015922}
+            fee = trade[trading_enums.ExchangeConstantsOrderColumns.FEE.value]
+            if trading_enums.ExchangeConstantsFeesColumns.CURRENCY.value not in fee:
+                fee[trading_enums.ExchangeConstantsFeesColumns.CURRENCY.value] = fee.get("code")
+            fee[trading_enums.ExchangeConstantsFeesColumns.COST.value] *= -1
+        return trades
 
     def get_market_status(self, symbol, price_example=None, with_fixer=True):
         try:
