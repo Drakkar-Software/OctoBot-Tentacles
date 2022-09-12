30fb023fa64672cd112936a57943bd9d8c7f2e08
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-09-12 14:35:30 +0200

    [Kucoin] handle 429000 error

--- Trading/Exchange/kucoin/kucoin_exchange.py
+++ Trading/Exchange/kucoin/kucoin_exchange.py
@@ -17,12 +17,37 @@ import copy
 
 import ccxt
 
+import octobot_commons.logging as logging
 import octobot_trading.errors
 import octobot_trading.enums as trading_enums
 import octobot_trading.exchanges as exchanges
 
 
+def _kucoin_retrier(f):
+    async def wrapper(*args, **kwargs):
+        for i in range(0, Kucoin.MAX_CANDLES_FETCH_INSTANT_RETRY):
+            try:
+                return await f(*args, **kwargs)
+            except octobot_trading.errors.FailedRequest as e:
+                if Kucoin.INSTANT_RETRY_ERROR_CODE in str(e):
+                    # should retry instantly, error on kucoin side
+                    # see https://github.com/Drakkar-Software/OctoBot/issues/2000
+                    logging.get_logger(Kucoin.get_name()).debug(
+                        f"{Kucoin.INSTANT_RETRY_ERROR_CODE} error on request, retrying now "
+                        f"(attempt {i+1} / {Kucoin.MAX_CANDLES_FETCH_INSTANT_RETRY}).")
+                else:
+                    raise
+        raise octobot_trading.errors.FailedRequest(
+            f"Failed request after {Kucoin.MAX_CANDLES_FETCH_INSTANT_RETRY} retries due "
+            f"to {Kucoin.INSTANT_RETRY_ERROR_CODE} error code"
+        )
+    return wrapper
+
+
 class Kucoin(exchanges.SpotCCXTExchange):
+    MAX_CANDLES_FETCH_INSTANT_RETRY = 5
+    INSTANT_RETRY_ERROR_CODE = "429000"
+
     @classmethod
     def get_name(cls):
         return 'kucoin'
@@ -30,6 +55,10 @@ class Kucoin(exchanges.SpotCCXTExchange):
     @classmethod
     def is_supporting_exchange(cls, exchange_candidate_name) -> bool:
         return cls.get_name() == exchange_candidate_name
+
+    @_kucoin_retrier
+    async def get_symbol_prices(self, symbol, time_frame, limit: int = 200, **kwargs: dict):
+        return await super().get_symbol_prices(symbol=symbol, time_frame=time_frame, limit=limit, **kwargs)
 
     async def get_recent_trades(self, symbol, limit=50, **kwargs):
         # on ccxt kucoin recent trades are received in reverse order from exchange and therefore should never be
@@ -61,3 +90,9 @@ class Kucoin(exchanges.SpotCCXTExchange):
             trading_enums.ExchangeConstantsMarketStatusColumns.LIMITS_PRICE.value][
             trading_enums.ExchangeConstantsMarketStatusColumns.LIMITS_PRICE_MAX.value] = None
         return market_status
+
+    def should_log_on_ddos_exception(self, exception) -> bool:
+        """
+        Override when necessary
+        """
+        return Kucoin.INSTANT_RETRY_ERROR_CODE not in str(exception)
