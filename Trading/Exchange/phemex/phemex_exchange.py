3cf3404967dffe05821ff88574ddc6079c78c8c3
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-09-24 10:19:13 +0200

    [Exchanges] add phemex

---
+++ Trading/Exchange/phemex/phemex_exchange.py
@@ -0,0 +1,61 @@
+#  Drakkar-Software OctoBot-Tentacles
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+import copy
+import math
+
+import ccxt
+
+import octobot_trading.errors
+import octobot_trading.enums as trading_enums
+import octobot_trading.exchanges as exchanges
+
+
+class Phemex(exchanges.SpotCCXTExchange):
+    DESCRIPTION = ""
+
+    @classmethod
+    def get_name(cls):
+        return 'phemex'
+
+    @classmethod
+    def is_supporting_exchange(cls, exchange_candidate_name) -> bool:
+        return cls.get_name() == exchange_candidate_name
+
+    def get_market_status(self, symbol, price_example=None, with_fixer=True):
+        try:
+            market_status = self._fix_market_status(copy.deepcopy(self.connector.client.market(symbol)))
+            if with_fixer:
+                market_status = exchanges.ExchangeMarketStatusFixer(market_status, price_example).market_status
+            return market_status
+        except ccxt.NotSupported:
+            raise octobot_trading.errors.NotSupported
+        except Exception as e:
+            self.logger.error(f"Fail to get market status of {symbol}: {e}")
+        return {}
+
+    def _fix_market_status(self, market_status):
+        market_status[trading_enums.ExchangeConstantsMarketStatusColumns.PRECISION.value][
+            trading_enums.ExchangeConstantsMarketStatusColumns.PRECISION_AMOUNT.value] = self._get_digits_count(
+            market_status[trading_enums.ExchangeConstantsMarketStatusColumns.PRECISION.value][
+                trading_enums.ExchangeConstantsMarketStatusColumns.PRECISION_AMOUNT.value])
+        market_status[trading_enums.ExchangeConstantsMarketStatusColumns.PRECISION.value][
+            trading_enums.ExchangeConstantsMarketStatusColumns.PRECISION_PRICE.value] = self._get_digits_count(
+            market_status[trading_enums.ExchangeConstantsMarketStatusColumns.PRECISION.value][
+                trading_enums.ExchangeConstantsMarketStatusColumns.PRECISION_PRICE.value])
+        return market_status
+
+    def _get_digits_count(self, value):
+        return round(abs(math.log(value, 10)))
