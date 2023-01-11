a859b30806e9fc4c75376d868e05d066dc54814d
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2023-01-11 13:26:00 +0100

    [Collector] update tests

--- Backtesting/collectors/exchanges/exchange_history_collector/tests/test_history_collector.py
+++ Backtesting/collectors/exchanges/exchange_history_collector/tests/test_history_collector.py
@@ -22,6 +22,7 @@ import asyncio
 import octobot_commons.databases as databases
 import octobot_commons.symbols as commons_symbols
 import octobot_commons.enums as commons_enums
+import octobot_commons.constants as commons_constants
 import octobot_backtesting.enums as enums
 import octobot_backtesting.errors as errors
 import octobot_trading.enums as trading_enums
@@ -111,7 +112,10 @@ async def test_collect_valid_date_range():
     symbols = ["ETH/BTC"]
     start_time = 1569413160000
     end_time = 1569914160000
-    async with data_collector(BINANCEUS, tentacles_setup_config, symbols, None, True, start_time, end_time) as collector:
+    # each request fetches 500 candles
+    candle_fetch_limit = 500
+    async with data_collector(BINANCEUS, tentacles_setup_config, symbols, None, True, start_time,
+                              end_time) as collector:
         assert collector.start_timestamp is not None
         assert collector.end_timestamp is not None
         await collector.start()
@@ -125,17 +129,45 @@ async def test_collect_valid_date_range():
         async with collector_database(collector) as database:
             ohlcv = await database.select(enums.ExchangeDataTables.OHLCV)
             assert len(ohlcv) == 13943
-            for index, candle in enumerate(ohlcv):
-                candle_open_time = json.loads(candle[-1])[commons_enums.PriceIndexes.IND_PRICE_TIME.value]
+            parsed_candles = [
+                json.loads(candle[-1])
+                for candle in ohlcv
+            ]
+            for parsed_candle in parsed_candles:
+                candle_open_time = parsed_candle[commons_enums.PriceIndexes.IND_PRICE_TIME.value]
                 assert start_time <= candle_open_time * 1000 <= end_time
-            h_ohlcv = await database.select(enums.ExchangeDataTables.OHLCV, time_frame="1h")
+            for time_frame in commons_enums.TimeFrames:
+                time_frame_ohlcv = await database.select(enums.ExchangeDataTables.OHLCV, time_frame=time_frame.value)
+                if not time_frame_ohlcv:
+                    continue
+                all_timestamps = sorted([
+                    candle[commons_enums.PriceIndexes.IND_PRICE_TIME.value]
+                    for candle in (
+                        json.loads(candle[-1])
+                        for candle in time_frame_ohlcv
+                    )
+                ])
+                # ensure no duplicate
+                timestamps = set(all_timestamps)
+                assert len(timestamps) == len(time_frame_ohlcv)
+                # ensure no missing
+                interval = commons_enums.TimeFramesMinutes[time_frame] * commons_constants.MINUTE_TO_SECONDS
+                current_ts = all_timestamps[0] - interval
+                for timestamp in all_timestamps:
+                    current_ts += interval
+                    assert timestamp == current_ts
+
+            h_ohlcv = await database.select(enums.ExchangeDataTables.OHLCV,
+                                            time_frame=commons_enums.TimeFrames.ONE_HOUR.value)
             assert len(h_ohlcv) == 139
             eth_btc_ohlcv = await database.select(enums.ExchangeDataTables.OHLCV, symbol="ETH/BTC")
             assert len(eth_btc_ohlcv) == len(ohlcv)
             min_timestamp = (await database.select_min(enums.ExchangeDataTables.OHLCV, ["timestamp"],
-                                                       time_frame="1m"))[0][0]*1000
+                                                       time_frame=commons_enums.TimeFrames.ONE_MINUTE.value))[0][
+                                commons_enums.PriceIndexes.IND_PRICE_TIME.value] * 1000
             assert start_time <= min_timestamp <= start_time + (60 * 1000)
-            max_timestamp = (await database.select_max(enums.ExchangeDataTables.OHLCV, ["timestamp"]))[0][0]*1000
+            max_timestamp = (await database.select_max(enums.ExchangeDataTables.OHLCV, ["timestamp"]))[0][
+                                commons_enums.PriceIndexes.IND_PRICE_TIME.value] * 1000
             assert end_time <= max_timestamp <= end_time + (31 * 24 * 60 * 60 * 1000)
 
 
