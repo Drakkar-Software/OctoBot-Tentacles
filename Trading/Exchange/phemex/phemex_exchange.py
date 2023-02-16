639020f3019c821eaa436b5f0402f70ddbb22cc9
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2023-02-16 20:46:16 +0100

    [Exchanges] handle ccxt update

--- Trading/Exchange/phemex/phemex_exchange.py
+++ Trading/Exchange/phemex/phemex_exchange.py
@@ -14,7 +14,10 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 import asyncio
+import typing
 
+import octobot_commons.enums as commons_enums
+import octobot_commons.constants as commons_constants
 import octobot_trading.exchanges as exchanges
 
 
@@ -24,6 +27,24 @@ class Phemex(exchanges.RestExchange):
     @classmethod
     def get_name(cls):
         return 'phemex'
+
+    def _get_ohlcv_params(self, time_frame, limit, **kwargs):
+        to_time = self.connector.milliseconds()
+        time_frame_msec = commons_enums.TimeFramesMinutes[time_frame] * commons_constants.MSECONDS_TO_MINUTE
+        kwargs.update({
+            "from": to_time - (time_frame_msec * (limit + 1)),
+            "limit": limit,
+        })
+        return kwargs
+
+    async def get_symbol_prices(self, symbol: str, time_frame: commons_enums.TimeFrames, limit: int = None,
+                                **kwargs: dict) -> typing.Optional[list]:
+        # without limit is not supported
+        if limit is None:
+            limit = 100
+        return await super().get_symbol_prices(
+            symbol=symbol, time_frame=time_frame, **self._get_ohlcv_params(time_frame, limit, **kwargs)
+        )
 
     async def get_order(self, order_id: str, symbol: str = None, **kwargs: dict) -> dict:
         if order := await self.connector.get_order(symbol=symbol, order_id=order_id, **kwargs):
