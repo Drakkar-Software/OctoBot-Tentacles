4ef70a670269bf1ccafaa707e7062ed25232daa5
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2023-04-23 22:22:24 +0200

    [BacktestingAnalysis] improve display & fix multi symbol

--- Meta/Keywords/scripting_library/backtesting/default_backtesting_run_analysis_script.py
+++ Meta/Keywords/scripting_library/backtesting/default_backtesting_run_analysis_script.py
@@ -2,6 +2,7 @@ import datetime as datetime
 import json as json
 
 import octobot_commons.enums as commons_enums
+import octobot_commons.pretty_printer as pretty_printer
 import octobot_services.constants as services_constants
 import tentacles.Meta.Keywords.scripting_library.backtesting.run_data_analysis as run_data_analysis
 import octobot_trading.modes.script_keywords as script_keywords
@@ -11,42 +12,53 @@ async def default_backtesting_analysis_script(ctx: script_keywords.Context):
     async with ctx.backtesting_results() as (run_data, run_display):
         if ctx.backtesting_analysis_settings["plot_pnl_on_main_chart"]:
             with run_display.part("main-chart") as part:
-                await run_data_analysis.plot_historical_portfolio_value(run_data, part)
-                await run_data_analysis.plot_historical_pnl_value(
-                    run_data, part, x_as_trade_count=False,
-                    own_yaxis=True,
-                    include_unitary=ctx.backtesting_analysis_settings["plot_trade_gains_on_main_chart"]
-                )
-
+                try:
+                    await run_data_analysis.plot_historical_portfolio_value(run_data, part)
+                    await run_data_analysis.plot_historical_pnl_value(
+                        run_data, part, x_as_trade_count=False,
+                        own_yaxis=True,
+                        include_unitary=ctx.backtesting_analysis_settings["plot_trade_gains_on_main_chart"]
+                    )
+                except Exception as err:
+                    ctx.logger.exception(err, True, f"Error when computing main chant graphs {err}")
         with run_display.part("backtesting-run-overview") as part:
-            if ctx.backtesting_analysis_settings.get("plot_hist_portfolio_on_backtesting_chart", True):
-                await run_data_analysis.plot_historical_portfolio_value(run_data, part)
-            if ctx.backtesting_analysis_settings["plot_pnl_on_backtesting_chart"]:
-                await run_data_analysis.plot_historical_pnl_value(
-                    run_data, part, x_as_trade_count=False,
-                    own_yaxis=True,
-                    include_unitary=ctx.backtesting_analysis_settings["plot_trade_gains_on_backtesting_chart"]
-                )
-            if ctx.backtesting_analysis_settings["plot_best_case_growth_on_backtesting_chart"]:
-                await run_data_analysis.plot_best_case_growth(run_data, part, x_as_trade_count=True, own_yaxis=False)
-            if ctx.backtesting_analysis_settings["plot_funding_fees_on_backtesting_chart"]:
-                await run_data_analysis.plot_historical_funding_fees(run_data, part, own_yaxis=True)
-            if ctx.backtesting_analysis_settings["plot_wins_and_losses_count_on_backtesting_chart"]:
-                await run_data_analysis.plot_historical_wins_and_losses(run_data, part, own_yaxis=True,
-                                                                        x_as_trade_count=False)
-            if ctx.backtesting_analysis_settings["plot_win_rate_on_backtesting_chart"]:
-                await run_data_analysis.plot_historical_win_rates(run_data, part, own_yaxis=True,
-                                                                  x_as_trade_count=False)
-            # await plot_withdrawals(run_data, part)
+            try:
+                if ctx.backtesting_analysis_settings.get("plot_hist_portfolio_on_backtesting_chart", True):
+                    await run_data_analysis.plot_historical_portfolio_value(run_data, part)
+                if ctx.backtesting_analysis_settings["plot_pnl_on_backtesting_chart"]:
+                    await run_data_analysis.plot_historical_pnl_value(
+                        run_data, part, x_as_trade_count=False,
+                        own_yaxis=True,
+                        include_unitary=ctx.backtesting_analysis_settings["plot_trade_gains_on_backtesting_chart"]
+                    )
+                if ctx.backtesting_analysis_settings["plot_best_case_growth_on_backtesting_chart"]:
+                    await run_data_analysis.plot_best_case_growth(run_data, part, x_as_trade_count=True, own_yaxis=False)
+                if ctx.backtesting_analysis_settings["plot_funding_fees_on_backtesting_chart"]:
+                    await run_data_analysis.plot_historical_funding_fees(run_data, part, own_yaxis=True)
+                if ctx.backtesting_analysis_settings["plot_wins_and_losses_count_on_backtesting_chart"]:
+                    await run_data_analysis.plot_historical_wins_and_losses(run_data, part, own_yaxis=True,
+                                                                            x_as_trade_count=False)
+                if ctx.backtesting_analysis_settings["plot_win_rate_on_backtesting_chart"]:
+                    await run_data_analysis.plot_historical_win_rates(run_data, part, own_yaxis=True,
+                                                                      x_as_trade_count=False)
+                # await plot_withdrawals(run_data, part)
+            except Exception as err:
+                ctx.logger.exception(err, True, f"Error when computing run overview graphs {err}")
         if ctx.backtesting_analysis_settings["display_backtest_details"]:
             with run_display.part("backtesting-details", "value") as part:
-                backtesting_report = await get_backtesting_report_template(run_data, ctx.backtesting_analysis_settings)
-                await run_data_analysis.display_html(part, backtesting_report)
+                try:
+                    backtesting_report = await get_backtesting_report_template(run_data, ctx.backtesting_analysis_settings)
+                    await run_data_analysis.display_html(part, backtesting_report)
+                except Exception as err:
+                    ctx.logger.exception(err, True, f"Error when computing details part {err}")
         if ctx.backtesting_analysis_settings["display_trades_and_positions"]:
             with run_display.part("list-of-trades-part", "table") as part:
-                await run_data_analysis.plot_trades(run_data, part)
-                await run_data_analysis.plot_positions(run_data, part)
-                # await plot_table(run_data, part, "SMA 1")  # plot any cache key as a table
+                try:
+                    await run_data_analysis.plot_trades(run_data, part)
+                    await run_data_analysis.plot_positions(run_data, part)
+                    # await plot_table(run_data, part, "SMA 1")  # plot any cache key as a table
+                except Exception as err:
+                    ctx.logger.exception(err, True, f"Error when computing trades part {err}")
     return run_display
 
 
@@ -58,22 +70,9 @@ async def get_backtesting_report_template(run_data, backtesting_analysis_setting
     paid_fees_display = get_column_display(services_constants.PAID_FEES_STR,
                                            metadata["paid_fees"]) if "paid_fees" in metadata.keys() else ""
     performance_summary = ""
-    if backtesting_analysis_settings.get("display_backtest_details_general", True):
-        performance_summary \
-            = get_section_display("General",
-                                  get_column_display(commons_enums.BacktestingMetadata.NAME.value,
-                                                     metadata[commons_enums.BacktestingMetadata.NAME.value])
-                                  + get_column_display(commons_enums.BacktestingMetadata.OPTIMIZATION_CAMPAIGN.value,
-                                                       metadata[commons_enums.BacktestingMetadata.
-                                                       OPTIMIZATION_CAMPAIGN.value])
-                                  + optimizer_id_display
-                                  + get_column_display(commons_enums.BacktestingMetadata.ID.value,
-                                                       metadata[commons_enums.BacktestingMetadata.ID.value])
-                                  + get_column_display(commons_enums.DBRows.EXCHANGES.value,
-                                                       metadata[commons_enums.DBRows.EXCHANGES.value])
-                                  + get_column_display(commons_enums.BacktestingMetadata.BACKTESTING_FILES.value,
-                                                       metadata[commons_enums.BacktestingMetadata.BACKTESTING_FILES.value]))
+    reference_market = metadata[commons_enums.DBRows.REFERENCE_MARKET.value]
     if backtesting_analysis_settings.get("display_backtest_details_performances", True):
+        start_portfolio_value, end_portfolio_value = await run_data_analysis.get_portfolio_values(run_data)
         performance_summary \
             += get_section_display("Performance",
                                    get_column_display(commons_enums.BacktestingMetadata.START_PORTFOLIO.value,
@@ -84,9 +83,15 @@ async def get_backtesting_report_template(run_data, backtesting_analysis_setting
                                                       get_portfolio_display(
                                                           metadata[
                                                               commons_enums.BacktestingMetadata.END_PORTFOLIO.value])) +
+                                   get_column_display(f"{commons_enums.BacktestingMetadata.START_PORTFOLIO.value} "
+                                                      f"{reference_market} value",
+                                                      start_portfolio_value) +
+                                   get_column_display(f"{commons_enums.BacktestingMetadata.END_PORTFOLIO.value} "
+                                                      f"{reference_market} value",
+                                                      end_portfolio_value) +
                                    get_column_display(commons_enums.BacktestingMetadata.PERCENT_GAINS.value,
                                                       metadata[commons_enums.BacktestingMetadata.PERCENT_GAINS.value]) +
-                                   get_column_display(commons_enums.BacktestingMetadata.GAINS.value,
+                                   get_column_display(f"{reference_market} {commons_enums.BacktestingMetadata.GAINS.value}",
                                                       metadata[commons_enums.BacktestingMetadata.GAINS.value]) +
                                    get_column_display(
                                        commons_enums.BacktestingMetadata.TRADES.value + " (entries and exits)",
@@ -107,7 +112,21 @@ async def get_backtesting_report_template(run_data, backtesting_analysis_setting
                                            .COEFFICIENT_OF_DETERMINATION_MAX_BALANCE.value]) +
                                    paid_fees_display
                                    )
-
+    if backtesting_analysis_settings.get("display_backtest_details_general", True):
+        performance_summary \
+            += get_section_display("General",
+                                  get_column_display(commons_enums.BacktestingMetadata.NAME.value,
+                                                     metadata[commons_enums.BacktestingMetadata.NAME.value])
+                                  + get_column_display(commons_enums.BacktestingMetadata.OPTIMIZATION_CAMPAIGN.value,
+                                                       metadata[commons_enums.BacktestingMetadata.
+                                                       OPTIMIZATION_CAMPAIGN.value])
+                                  + optimizer_id_display
+                                  + get_column_display(commons_enums.BacktestingMetadata.ID.value,
+                                                       metadata[commons_enums.BacktestingMetadata.ID.value])
+                                  + get_column_display(commons_enums.DBRows.EXCHANGES.value,
+                                                       metadata[commons_enums.DBRows.EXCHANGES.value])
+                                  + get_column_display(commons_enums.BacktestingMetadata.BACKTESTING_FILES.value,
+                                                       metadata[commons_enums.BacktestingMetadata.BACKTESTING_FILES.value]))
     if backtesting_analysis_settings.get("display_backtest_details_details", True):
         performance_summary \
             += get_section_display("Details",
@@ -123,7 +142,7 @@ async def get_backtesting_report_template(run_data, backtesting_analysis_setting
                                    get_column_display(commons_enums.BacktestingMetadata.SYMBOLS.value,
                                                       get_badges_from_list(
                                                           metadata[commons_enums.BacktestingMetadata.SYMBOLS.value])) +
-                                   get_column_display(commons_enums.BacktestingMetadata.DURATION.value,
+                                   get_column_display(f"{commons_enums.BacktestingMetadata.DURATION.value} (s)",
                                                       metadata[commons_enums.BacktestingMetadata.DURATION.value]) +
                                    get_column_display(commons_enums.BacktestingMetadata.LEVERAGE.value,
                                                       metadata[commons_enums.BacktestingMetadata.LEVERAGE.value]) +
@@ -155,11 +174,13 @@ def get_section_display(title, content):
 def get_column_display(title, value):
     return f'''
         <div class="col-6 col-md-3  text-center">
-            <div class="backtesting-run-container-values-label">
+            <div class="backtesting-run-container-values-label text-capitalize">
                 {title}
             </div>
             <div class="backtesting-run-container-values-value">
-                {value}
+                {', '.join(value) if (isinstance(value, list) and value and isinstance(value[0], (int, float, str)))
+                    else pretty_printer.get_min_string_from_number(value) if isinstance(value, float) 
+                    else value}
             </div>
         </div>
     '''
@@ -168,7 +189,7 @@ def get_column_display(title, value):
 def get_badges_from_list(_list):
     _html = ""
     for _item in _list:
-        _html += f'<span class="badge badge-primary">{_item}</span>'
+        _html += f'<span class="badge badge-light mx-1">{_item}</span>'
     return _html
 
 
@@ -177,7 +198,7 @@ def get_portfolio_display(_dict):
     _dict_str = _dict.replace("\'", '"')
     _dict_str = json.loads(_dict_str)
     for _key in _dict_str:
-        _html += f'<span class="mx-1">{_key}: {round(_dict_str[_key]["total"], 6)}</span>'
+        _html += f'<span class="mx-1">{_key}: {pretty_printer.get_min_string_from_number(_dict_str[_key]["total"])}</span>'
     return _html
 
 
