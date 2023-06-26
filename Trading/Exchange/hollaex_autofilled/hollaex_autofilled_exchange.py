57c7efaa5803c5aa0beb0d6cc56565e28f61aab3
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2023-06-26 20:36:43 +0200

    [AutoFilledExchanges] support websockets

--- Trading/Exchange/hollaex_autofilled/hollaex_autofilled_exchange.py
+++ Trading/Exchange/hollaex_autofilled/hollaex_autofilled_exchange.py
@@ -61,7 +61,7 @@ class HollaexAutofilled(hollaex):
 
     @staticmethod
     def _has_websocket(tentacle_config, exchange_name):
-        return HollaexAutofilled._get_autofilled_config(tentacle_config, exchange_name)["websockets"]
+        return HollaexAutofilled._get_autofilled_config(tentacle_config, exchange_name).get("websockets", False)
 
     @staticmethod
     def _get_autofilled_config(tentacle_config, exchange_name):
@@ -69,6 +69,118 @@ class HollaexAutofilled(hollaex):
 
     @classmethod
     def _parse_autofilled_exchange_details(cls, tentacle_config, kit_details, exchange_name):
+        """
+        use /kit to fill in exchange details
+        format:
+        {
+            "api_name": "BitcoinRD Exchange",
+            "black_list_countries": [],
+            "captcha": {},
+            "color": {
+                "Black": {
+                    "base_background": "#000000",
+                    ...
+                }
+            },
+            "defaults": {
+                "country": "DO",
+                "language": "es",
+                "theme": "dark"
+            },
+            "description": "Primer Exchange 100% Dominicano.",
+            "dust": {
+                "maker_id": 1,
+                "quote": "xht",
+                "spread": 0
+            },
+            "email_verification_required": true,
+            "features": {
+                "chat": false,
+                ...
+            },
+            "icons": {
+                "dark": {
+                    "DOP_ICON": "https://bitholla.s3.ap-northeast-2.amazonaws.com/exchange/bitcoinrdexchange/DOP_ICON__dark___1631209668172.png",
+                    ...
+                },
+                "white": {
+                    "EXCHANGE_FAV_ICON": "https://bitholla.s3.ap-northeast-2.amazonaws.com/exchange/bitcoinrdexchange/EXCHANGE_FAV_ICON__white___1615349464540.png",
+                    ...
+                }
+            },
+            "info": {
+                "active": true,
+                "collateral_level": "member",
+                "created_at": "2021-03-09T14:12:49.012Z",
+                "exchange_id": 1512,
+                "expiry": "2023-08-27T23:59:59.000Z",
+                "initialized": true,
+                "is_trial": false,
+                "name": "bitcoinrdexchange",
+                "period": "year",
+                "plan": "fiat",
+                "status": true,
+                "type": "Cloud",
+                "url": "https://api.bitcoinrd.do",
+                "user_id": 3536
+            },
+            "injected_html": {
+                "body": "",
+                "head": ""
+            },
+            "injected_values": [],
+            "interface": {},
+            "links": {
+                "api": "https://api.bitcoinrd.do",
+                "contact": "",
+                "facebook": "",
+                "github": "",
+                "helpdesk": "mailto:soporte@bitcoinrd.do",
+                "information": "",
+                "instagram": "",
+                "linkedin": "",
+                "privacy": "https://bitcoinrd.online/privacy-policy/",
+                "referral_label": "Powered by BitcoinRD",
+                "referral_link": "https://bitcoinrd.online/",
+                "section_1": {
+                    "content": {
+                        "instagram": "https://www.instagram.com/bitcoinrd/"
+                    },
+                    "header": {
+                        "column_header_1": "RRSS"
+                    }
+                },
+                "section_2": "",
+                "telegram": "",
+                "terms": "https://bitcoinrd.online/terms/",
+                "twitter": "",
+                "website": "",
+                "whitepaper": ""
+            },
+            "logo_image": "https://bitholla.s3.ap-northeast-2.amazonaws.com/exchange/bitcoinrdexchange/EXCHANGE_LOGO__dark___1615345052424.png",
+            "meta": {
+                "default_digital_assets_sort": "change",
+                ...
+                },
+                "versions": {
+                    "color": "color-1681492596812",
+                    ...
+                }
+            },
+            "native_currency": "usdt",
+            "new_user_is_activated": true,
+            "offramp": {...},
+            "onramp": {...},
+            "setup_completed": true,
+            "strings": {
+                "en": { ...}
+            },
+            "title": "",
+            "user_meta": {},
+            "user_payments": {},
+            "valid_languages": "en,es,fr"
+        }
+        """
         return exchanges.ExchangeDetails(
             exchange_name,
             kit_details["api_name"],
@@ -86,8 +198,15 @@ class HollaexAutofilled(hollaex):
         self.tentacle_config[self.REST_KEY] = autofilled_exchange_details.api
         self.tentacle_config[self.HAS_WEBSOCKETS_KEY] = autofilled_exchange_details.has_websocket
 
+    @classmethod
+    def is_supporting_sandbox(cls) -> bool:
+        return False
+
     def get_rest_name(self):
         return hollaex.get_name()
+
+    def get_associated_websocket_exchange_name(self):
+        return self.get_name()
 
     @classmethod
     def get_name(cls):
