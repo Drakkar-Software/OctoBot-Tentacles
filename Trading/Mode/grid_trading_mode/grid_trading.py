28336777b1989cbbe6bb824bb7353782fcf41a21
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2023-07-28 19:09:24 +0200

    [GridTrading] remove portfolio lock

--- Trading/Mode/grid_trading_mode/grid_trading.py
+++ Trading/Mode/grid_trading_mode/grid_trading.py
@@ -284,11 +284,9 @@ class GridTradingModeProducer(staggered_orders_trading.StaggeredOrdersTradingMod
     async def _handle_staggered_orders(self, current_price, ignore_mirror_orders_only, ignore_available_funds):
         self._init_allowed_price_ranges(current_price)
         if ignore_mirror_orders_only or not self.use_existing_orders_only:
-            # ignoring existing orders ?
             buy_orders, sell_orders = await self._generate_staggered_orders(current_price, ignore_available_funds)
             grid_orders = self._merged_and_sort_not_virtual_orders(buy_orders, sell_orders)
-            async with self.exchange_manager.exchange_personal_data.portfolio_manager.portfolio.lock:
-                await self._create_not_virtual_orders(grid_orders, current_price)
+            await self._create_not_virtual_orders(grid_orders, current_price)
 
     async def trigger_staggered_orders_creation(self):
         # reload configuration
