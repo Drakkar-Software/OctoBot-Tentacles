47d199b9380635733512e05342dcbb67cff1eefc
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2023-09-25 20:24:44 +0200

    [WebInterface] disable beta tentacles

--- Services/Interfaces/web_interface/flask_util/context_processor.py
+++ Services/Interfaces/web_interface/flask_util/context_processor.py
@@ -127,6 +127,7 @@ def register_context_processor(web_interface_instance):
             OCTOBOT_FEEDBACK_URL=constants.OCTOBOT_FEEDBACK,
             OCTOBOT_CLOUD_URL=constants.OCTOBOT_CLOUD_URL,
             OCTOBOT_COMMUNITY_URL=identifiers_provider.IdentifiersProvider.COMMUNITY_URL,
+            OCTOBOT_COMMUNITY_LANDING_URL=identifiers_provider.IdentifiersProvider.COMMUNITY_LANDING_URL,
             OCTOBOT_COMMUNITY_RECOVER_PASSWORD_URL=identifiers_provider.IdentifiersProvider.FRONTEND_PASSWORD_RECOVER_URL,
             CURRENT_BOT_VERSION=interfaces.AbstractInterface.project_version,
             IS_DEMO=constants.IS_DEMO,
