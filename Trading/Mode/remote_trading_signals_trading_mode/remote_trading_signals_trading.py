d94e254c00d988c44c23de1d1024949b08f64498
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2023-10-03 19:02:18 +0200

    [RemoteTradingSignalsTradingMode] accuratly handle chained orders error

--- Trading/Mode/remote_trading_signals_trading_mode/remote_trading_signals_trading.py
+++ Trading/Mode/remote_trading_signals_trading_mode/remote_trading_signals_trading.py
@@ -275,15 +275,20 @@ class RemoteTradingSignalsModeConsumer(trading_modes.AbstractTradingModeConsumer
 
     async def _chain_order(self, order_description, created_orders, ignored_orders, chained_to, fees_currency_side,
                            created_groups, symbol, order_description_by_id):
+        failed_order_creation = False
         try:
             base_order = created_orders[chained_to]
             if base_order is None:
                 # when an error occurred when creating the initial order
+                failed_order_creation = True
                 raise KeyError
         except KeyError as e:
-            if chained_to in ignored_orders:
-                self.logger.error(f"Ignored order chained to id {chained_to}: "
-                                  f"associated master order has not been created")
+            if chained_to in ignored_orders or failed_order_creation:
+                message = f"Ignored order chained to id {chained_to}: associated master order has not been created"
+                if failed_order_creation:
+                    self.logger.info(message)
+                else:
+                    self.logger.error(message)
             else:
                 self.logger.error(
                     f"Ignored chained order from {order_description}. Chained orders have to be sent in the same "
