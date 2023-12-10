05d902c219dec8510d89877ac72059f06b8be345
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2023-12-10 11:55:24 +0100

    [Bingx] update after tests

--- Trading/Exchange/bingx/bingx_exchange.py
+++ Trading/Exchange/bingx/bingx_exchange.py
@@ -13,15 +13,12 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
-import typing
-
-import octobot_commons.enums
-
 import octobot_trading.exchanges as exchanges
+import octobot_trading.exchanges.connectors.ccxt.constants as ccxt_constants
+import octobot_trading.enums as trading_enums
 
 
 class Bingx(exchanges.RestExchange):
-    # ccxt 4.0.65: bingx websocket not yet supported
     FIX_MARKET_STATUS = True
 
     def get_adapter_class(self):
@@ -31,6 +28,36 @@ class Bingx(exchanges.RestExchange):
     def get_name(cls) -> str:
         return 'bingx'
 
+    def _fix_market_status(self, market_status, remove_price_limits=False):  # todo move to adapter
+        """
+        Overrite if necessary
+        """
+        # on bingx, amounts are precisions in the right unit, do not patch precision
+
+        if remove_price_limits:
+            market_status[trading_enums.ExchangeConstantsMarketStatusColumns.LIMITS.value][
+                trading_enums.ExchangeConstantsMarketStatusColumns.LIMITS_PRICE.value][
+                trading_enums.ExchangeConstantsMarketStatusColumns.LIMITS_PRICE_MIN.value] = None
+            market_status[trading_enums.ExchangeConstantsMarketStatusColumns.LIMITS.value][
+                trading_enums.ExchangeConstantsMarketStatusColumns.LIMITS_PRICE.value][
+                trading_enums.ExchangeConstantsMarketStatusColumns.LIMITS_PRICE_MAX.value] = None
+
+        return market_status
+
+    async def get_my_recent_trades(self, symbol=None, since=None, limit=None, **kwargs):
+        # On SPOT Bingx, account recent trades is available under fetch_closed_orders
+        if self.exchange_manager.is_future:
+            return await super().get_my_recent_trades(symbol=symbol, since=since, limit=limit, **kwargs)
+        return await super().get_closed_orders(symbol=symbol, since=since, limit=limit, **kwargs)
+
 
 class BingxCCXTAdapter(exchanges.CCXTAdapter):
-    pass
+
+    def fix_order(self, raw, **kwargs):
+        fixed = super().fix_order(raw, **kwargs)
+        try:
+            info = fixed[ccxt_constants.CCXT_INFO]
+            fixed[trading_enums.ExchangeConstantsOrderColumns.EXCHANGE_ID.value] = info["orderId"]
+        except KeyError:
+            pass
+        return fixed
