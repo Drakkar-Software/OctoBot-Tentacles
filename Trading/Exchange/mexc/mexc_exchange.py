1d57dc84eb013b1e6cdbc5fd557c20f530beef7a
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2025-11-07 16:19:12 +0100

    [Exchanges] handle forced auth

--- Trading/Exchange/mexc/mexc_exchange.py
+++ Trading/Exchange/mexc/mexc_exchange.py
@@ -18,6 +18,7 @@ import decimal
 import time
 import typing
 import ccxt
+import hashlib
 from ccxt.base.types import Entry
 
 import octobot_trading.exchanges as exchanges
@@ -30,7 +31,102 @@ import octobot_commons
 import octobot_trading.constants as constants
 
 
+class MEXCConnector(exchanges.CCXTConnector):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._force_signed_requests: typing.Optional[bool] = None
+
+    def _create_client(self, force_unauth=False):
+        super()._create_client(force_unauth=force_unauth)
+        self.client.sign = self._lazy_maybe_force_signed_requests(self.client.sign)
+
+    def _lazy_maybe_force_signed_requests(self, origin_ccxt_sign):
+        def lazy_sign(path, api, method, params, headers, body):
+            if self._force_signed_requests is None:
+                self._force_signed_requests = self.exchange_manager.exchange.requires_authentication(
+                    self.exchange_manager.exchange.tentacle_config, None, None
+                )
+            ccxt_sign_result = origin_ccxt_sign(path, api, method, params, headers, body)
+            if self._force_signed_requests:
+                url = ccxt_sign_result.get("url") or ""
+                ccxt_headers = ccxt_sign_result.get("headers") or {}
+                if "signature=" in url or "Signature" in ccxt_headers:
+                    # already signed
+                    return ccxt_sign_result
+                # force signature
+                return self._force_sign(path, api, method, params, headers, body)
+            return ccxt_sign_result
+        return lazy_sign
+
+    def _force_sign(self, path, api, method, params, headers, body):
+        self = self.client  # to use the same code as ccxt.async_support.mexc.sign (same self)
+        # same code as ccxt.async_support.mexc.sign but forced to sign
+        section = self.safe_string(api, 0)
+        access = self.safe_string(api, 1)
+        path, params = self.resolve_path(path, params)
+        url = None
+        if section == 'spot' or section == 'broker':
+            if section == 'broker':
+                url = self.urls['api'][section][access] + '/' + path
+            else:
+                url = self.urls['api'][section][access] + '/api/' + self.version + '/' + path
+            urlParams = params
+            if True or access == 'private':  # local override to force signature
+                if section == 'broker' and ((method == 'POST') or (method == 'PUT') or (method == 'DELETE')):
+                    urlParams = {
+                        'timestamp': self.nonce(),
+                        'recvWindow': self.safe_integer(self.options, 'recvWindow', 5000),
+                    }
+                    body = self.json(params)
+                else:
+                    urlParams['timestamp'] = self.nonce()
+                    urlParams['recvWindow'] = self.safe_integer(self.options, 'recvWindow', 5000)
+            paramsEncoded = ''
+            if urlParams:
+                paramsEncoded = self.urlencode(urlParams)
+                url += '?' + paramsEncoded
+            if True or access == 'private':  # local override to force signature
+                self.check_required_credentials()
+                signature = self.hmac(self.encode(paramsEncoded), self.encode(self.secret), hashlib.sha256)
+                url += '&' + 'signature=' + signature
+                headers = {
+                    'X-MEXC-APIKEY': self.apiKey,
+                    'source': self.safe_string(self.options, 'broker', 'CCXT'),
+                }
+            if (method == 'POST') or (method == 'PUT') or (method == 'DELETE'):
+                headers['Content-Type'] = 'application/json'
+        elif section == 'contract' or section == 'spot2':
+            url = self.urls['api'][section][access] + '/' + self.implode_params(path, params)
+            params = self.omit(params, self.extract_params(path))
+            if False and access == 'public':  # local override to force signature
+                if params:
+                    url += '?' + self.urlencode(params)
+            else:
+                self.check_required_credentials()
+                timestamp = str(self.nonce())
+                auth = ''
+                headers = {
+                    'ApiKey': self.apiKey,
+                    'Request-Time': timestamp,
+                    'Content-Type': 'application/json',
+                    'source': self.safe_string(self.options, 'broker', 'CCXT'),
+                }
+                if method == 'POST':
+                    auth = self.json(params)
+                    body = auth
+                else:
+                    params = self.keysort(params)
+                    if params:
+                        auth += self.urlencode(params)
+                        url += '?' + auth
+                auth = self.apiKey + timestamp + auth
+                signature = self.hmac(self.encode(auth), self.encode(self.secret), hashlib.sha256)
+                headers['Signature'] = signature
+        return {'url': url, 'method': method, 'body': body, 'headers': headers}
+
 class MEXC(exchanges.RestExchange):
+    DEFAULT_CONNECTOR_CLASS = MEXCConnector
     FIX_MARKET_STATUS = True
     REMOVE_MARKET_STATUS_PRICE_LIMITS = True
     # set True when disabled symbols should still be considered (ex: mexc with its temporary api trading disabled symbols)
